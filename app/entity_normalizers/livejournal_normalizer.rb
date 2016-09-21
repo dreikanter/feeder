7b7dba81c32a755e6a39b8f0395b96cd1c1da6f4
Alex Musayev <alex.musayev@gmail.com>
2016-09-21 10:37:49 +0300

    LJ normalizer updated

--- app/entity_normalizers/livejournal_normalizer.rb
+++ app/entity_normalizers/livejournal_normalizer.rb
@@ -1,8 +1,6 @@
 module EntityNormalizers
   class LivejournalNormalizer < EntityNormalizers::RssNormalizer
     # https://freefeed-product.hackpad.com/LtsQ2rM1RGD
-    MAX_COMMENT_LENGTH = 1500
-    OMISSION = '... (continued)'
 
     def text
       [entity.title, "!#{entity.link}"].reject(&:blank?).join(' - ')
@@ -13,19 +11,21 @@ module EntityNormalizers
     end
 
     def attachments
-      image ? [image] : []
+      [image]
+    rescue
+      []
     end
 
     def image
-      @image ||= description.
-        css('img:first').first.try(:[], 'src')
+      @image ||= description.css('img:first').first['src']
     end
 
     def excerpt
       result = description
       result.css('br').each { |br| br.replace "\n" }
       result.text.squeeze(" ").gsub(/\n{2,}/, "\n").
-        truncate(MAX_COMMENT_LENGTH, separator: ' ', omission: OMISSION)
+        truncate(Const::Content::MAX_COMMENT_LENGTH, separator: ' ',
+          omission: Const::Content::OMISSION)
     end
 
     def description
