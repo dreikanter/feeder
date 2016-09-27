b4cca85b0edfe98e8c67955e5fff87793a90ee7d
Alex Musayev <alex.musayev@gmail.com>
2016-09-27 19:11:08 +0300

    GitHub Blog normalizer

---
+++ app/entity_normalizers/github_blog_normalizer.rb
@@ -0,0 +1,25 @@
+module EntityNormalizers
+  class GithubBlogNormalizer < EntityNormalizers::AtomNormalizer
+    def text
+      "#{super} - !#{link}"
+    end
+
+    def comments
+      [content_excerpt]
+    end
+
+    def attachments
+      [first_image_url]
+    end
+
+    private
+
+    def content_excerpt
+      Service::Html.excerpt(entity.content.content)
+    end
+
+    def first_image_url
+      Service::Html.first_image_url(entity.content.content)
+    end
+  end
+end
