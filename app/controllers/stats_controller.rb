fdebb7b5b4dd67f2e9d9c81b11d95cf66d46a63b
Alex Musayev <alex.musayev@gmail.com>
2017-02-14 20:37:50 +0300

    Better history data preprocessing

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,18 +1,12 @@
 class StatsController < ApplicationController
   def show
     @feeds = Feed.all
-    @history = history
+    @history = recent_posts.group('DATE(created_at)').count.sort.to_h
   end
 
   private
 
-  def history
-    counters = {}
-    DataPoint.for('pull').where('created_at > ?', 30.days.ago).each do |dp|
-      key = dp.created_at.strftime("%Y/%m/%d")
-      counters[key] ||= 0
-      counters[key] += dp.details['posts_count'].to_i
-    end
-    counters
+  def recent_posts
+    Post.where('created_at > ?', 30.days.ago)
   end
 end
