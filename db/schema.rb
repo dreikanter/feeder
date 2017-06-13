e4d89c3958f1addbe5d2a7901b257d6a5ab26081
Alex Musayev <alex.musayev@gmail.com>
2017-06-13 14:40:53 +0300

    Schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -15,79 +15,79 @@ ActiveRecord::Schema.define(version: 20170308200158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "data_point_series", force: :cascade do |t|
-    t.string   "name",       null: false
+  create_table "data_point_series", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
-    t.index ["name"], name: "index_data_point_series_on_name", unique: true, using: :btree
+    t.index ["name"], name: "index_data_point_series_on_name", unique: true
   end
 
-  create_table "data_points", force: :cascade do |t|
-    t.integer  "series_id"
-    t.json     "details",    default: {}, null: false
-    t.datetime "created_at",              null: false
-    t.index ["series_id"], name: "index_data_points_on_series_id", using: :btree
+  create_table "data_points", id: :serial, force: :cascade do |t|
+    t.integer "series_id"
+    t.json "details", default: {}, null: false
+    t.datetime "created_at", null: false
+    t.index ["series_id"], name: "index_data_points_on_series_id"
   end
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
+  create_table "delayed_jobs", id: :serial, force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
+    t.string "locked_by"
+    t.string "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "errors", force: :cascade do |t|
-    t.integer  "status",             default: 0,  null: false
-    t.string   "error_class_name",   default: "", null: false
-    t.string   "file_name"
-    t.integer  "line_number"
-    t.string   "label",              default: "", null: false
-    t.string   "message",            default: "", null: false
-    t.string   "backtrace",          default: [], null: false, array: true
-    t.string   "filtered_backtrace", default: [], null: false, array: true
-    t.json     "context",            default: {}, null: false
-    t.datetime "occured_at",                      null: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.index ["error_class_name"], name: "index_errors_on_error_class_name", using: :btree
-    t.index ["file_name"], name: "index_errors_on_file_name", using: :btree
-    t.index ["occured_at"], name: "index_errors_on_occured_at", using: :btree
-    t.index ["status"], name: "index_errors_on_status", using: :btree
+  create_table "errors", id: :serial, force: :cascade do |t|
+    t.integer "status", default: 0, null: false
+    t.string "error_class_name", default: "", null: false
+    t.string "file_name"
+    t.integer "line_number"
+    t.string "label", default: "", null: false
+    t.string "message", default: "", null: false
+    t.string "backtrace", default: [], null: false, array: true
+    t.string "filtered_backtrace", default: [], null: false, array: true
+    t.json "context", default: {}, null: false
+    t.datetime "occured_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["error_class_name"], name: "index_errors_on_error_class_name"
+    t.index ["file_name"], name: "index_errors_on_file_name"
+    t.index ["occured_at"], name: "index_errors_on_occured_at"
+    t.index ["status"], name: "index_errors_on_status"
   end
 
-  create_table "feeds", force: :cascade do |t|
-    t.string   "name",                     null: false
-    t.integer  "posts_count",  default: 0, null: false
+  create_table "feeds", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "posts_count", default: 0, null: false
     t.datetime "refreshed_at"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "url"
-    t.string   "processor"
-    t.string   "normalizer"
-    t.index ["name"], name: "index_feeds_on_name", unique: true, using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
+    t.string "processor"
+    t.string "normalizer"
+    t.index ["name"], name: "index_feeds_on_name", unique: true
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.integer  "feed_id",                       null: false
-    t.string   "link",                          null: false
-    t.datetime "published_at",                  null: false
-    t.string   "text",             default: "", null: false
-    t.string   "attachments",      default: [], null: false, array: true
-    t.string   "comments",         default: [], null: false, array: true
-    t.string   "freefeed_post_id"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.integer  "status",           default: 0,  null: false
-    t.index ["feed_id"], name: "index_posts_on_feed_id", using: :btree
-    t.index ["link"], name: "index_posts_on_link", using: :btree
-    t.index ["status"], name: "index_posts_on_status", using: :btree
+  create_table "posts", id: :serial, force: :cascade do |t|
+    t.integer "feed_id", null: false
+    t.string "link", null: false
+    t.datetime "published_at", null: false
+    t.string "text", default: "", null: false
+    t.string "attachments", default: [], null: false, array: true
+    t.string "comments", default: [], null: false, array: true
+    t.string "freefeed_post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
+    t.index ["feed_id"], name: "index_posts_on_feed_id"
+    t.index ["link"], name: "index_posts_on_link"
+    t.index ["status"], name: "index_posts_on_status"
   end
 
 end
