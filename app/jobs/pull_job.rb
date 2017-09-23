1ff385a2d23642d9226563319784c43fa9c1d3be
Alex Musayev <alex.musayev@gmail.com>
2017-09-23 19:31:17 +0300

    404 exception handling

--- app/jobs/pull_job.rb
+++ app/jobs/pull_job.rb
@@ -15,7 +15,7 @@ class PullJob < ApplicationJob
     normalizer = Service::FeedNormalizer.for(feed)
     logger.info "---> normalizer: #{normalizer.entity_normalizer}"
 
-    Service::FeedLoader.load(feed_name).each do |link, entity|
+    load_entities(feed_name).each do |link, entity|
       begin
         logger.info "---> processing next entity #{'-' * 50}"
         next if Post.exists?(feed: feed, link: link)
@@ -48,5 +48,19 @@ class PullJob < ApplicationJob
     )
 
     DataPoint.purge_old_records!
+  end
+
+  private
+
+  def load_entities(feed_name)
+    Service::FeedLoader.load(feed_name)
+  rescue => exception
+    logger.error "---> error loading feed: #{exception.message}"
+    Error.dump(exception, context: {
+      class_name: self.class.name,
+      feed_name: feed_name,
+      hint: 'error loading feed'
+    })
+    []
   end
 end
