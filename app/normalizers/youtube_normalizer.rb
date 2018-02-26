fe5cf923576e7be4f762177e392cc3b93d4e14e3
Alex Musayev <alex.musayev@gmail.com>
2018-02-26 22:05:55 +0300

    Youtube processor and normalizer

---
+++ app/normalizers/youtube_normalizer.rb
@@ -0,0 +1,19 @@
+module Normalizers
+  class YoutubeNormalizer < Normalizers::Base
+    def link
+      entity.url
+    end
+
+    def published_at
+      entity.published
+    end
+
+    def text
+      [entity.title, entity.url].join(separator)
+    end
+
+    def comments
+      [entity.content]
+    end
+  end
+end
---
+++ app/processors/youtube_processor.rb
@@ -0,0 +1,16 @@
+module Processors
+  class YoutubeProcessor < Processors::Base
+    def entities
+      parse_source.map { |entity| [entity.url, entity] }
+    end
+
+    private
+
+    def parse_source
+      Feedjira::Feed.parse(source).entries
+    rescue => e
+      Rails.logger.error "error parsing youtube feed: #{e}"
+      []
+    end
+  end
+end
