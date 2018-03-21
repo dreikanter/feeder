4f59933aa14e48c497ff3247f41cb21d034b6432
Alex Musayev <alex.musayev@gmail.com>
2018-03-21 21:36:09 +0300

    4 more feeds added
    Blitz and Chips, Jimmi Diresta, Confreaks, and they can talk

---
+++ app/normalizers/theycantalk_normalizer.rb
@@ -0,0 +1,29 @@
+module Normalizers
+  class TheycantalkNormalizer < Normalizers::TumblrNormalizer
+    def text
+      [post_excerpt, link].reject(&:blank?).join(separator)
+    end
+
+    def attachments
+      [ image_url ]
+    end
+
+    def comments
+      paragraphs[1..-1] || []
+    end
+
+    private
+
+    def image_url
+      Service::Html.first_image_url(entity.description)
+    end
+
+    def post_excerpt
+      Service::Html.post_excerpt(paragraphs.first)
+    end
+
+    def paragraphs
+      @paragraphs ||= Service::Html.paragraphs(entity.description)
+    end
+  end
+end
