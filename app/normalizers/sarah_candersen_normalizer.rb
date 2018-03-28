eceb2b6981052ffd9080eefd86bfcb1e3d0b02d8
Alex Musayev <alex.musayev@gmail.com>
2018-03-28 03:40:47 +0300

    Sarah Andersen

---
+++ app/normalizers/sarah_candersen_normalizer.rb
@@ -0,0 +1,21 @@
+module Normalizers
+  class SarahCandersenNormalizer < Normalizers::TumblrNormalizer
+    def text
+      [post_excerpt, link].reject(&:blank?).join(separator)
+    end
+
+    def attachments
+      [image_url]
+    end
+
+    private
+
+    def image_url
+      Service::Html.first_image_url(entity.description)
+    end
+
+    def post_excerpt
+      Service::Html.post_excerpt(entity.description)
+    end
+  end
+end
