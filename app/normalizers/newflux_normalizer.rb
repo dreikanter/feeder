ae5631656ee840e9f628c43803d77395c90f407c
Alex Musayev <alex.musayev@gmail.com>
2018-04-05 21:31:51 +0300

    Add first image to the attachments

--- app/normalizers/newflux_normalizer.rb
+++ app/normalizers/newflux_normalizer.rb
@@ -4,10 +4,24 @@ module Normalizers
       [summary]
     end
 
+    def attachments
+      [image_url]
+    end
+
     private
 
     def summary
       Service::Html.comment_excerpt(entity.summary)
+    end
+
+    COVER_QUERY = 'meta[name="twitter:image"]'
+
+    def image_url
+      Nokogiri::HTML(page_content).css(COVER_QUERY).first[:content]
+    end
+
+    def page_content
+      RestClient.get(link).body
     end
   end
 end
