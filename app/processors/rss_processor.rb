664fe6fa820ae150f7f41afff7cb61898b9d97e4
Alex Musayev <alex.musayev@gmail.com>
2018-04-11 19:08:31 +0300

    Nil check

--- app/processors/rss_processor.rb
+++ app/processors/rss_processor.rb
@@ -3,10 +3,10 @@ require 'rss'
 module Processors
   class RssProcessor < Processors::Base
     def entities
-      items = RSS::Parser.parse(source).items
       Rails.logger.warn "RSS::Parser.parse().items returned nil"
       return [] unless items
       items.map { |i| [i.link, i] }
+      items = RSS::Parser.parse(source).try(:items)
     end
   end
 end
