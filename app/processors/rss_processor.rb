0d85b4fcad694d9d325f0f9c59cb893fff9e614b
Alex Musayev <alex.musayev@gmail.com>
2018-04-11 19:08:40 +0300

    Logging fixed

--- app/processors/rss_processor.rb
+++ app/processors/rss_processor.rb
@@ -3,10 +3,9 @@ require 'rss'
 module Processors
   class RssProcessor < Processors::Base
     def entities
-      Rails.logger.warn "RSS::Parser.parse().items returned nil"
-      return [] unless items
-      items.map { |i| [i.link, i] }
       items = RSS::Parser.parse(source).try(:items)
+      Rails.logger.warn("RSS has no items") unless items
+      (items || []).map { |i| [i.link, i] }
     end
   end
 end
