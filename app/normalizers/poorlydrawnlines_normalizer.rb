5c66912aa0ee02c1aa24e7d2769169139fa8eecc
Alex Musayev <alex.musayev@gmail.com>
2018-10-23 17:08:29 +0300

    Fix #64: Poorly Drawn Lines

---
+++ app/normalizers/poorlydrawnlines_normalizer.rb
@@ -0,0 +1,25 @@
+module Normalizers
+  class PoorlydrawnlinesNormalizer < Normalizers::Base
+    def link
+      entity.url
+    end
+
+    def published_at
+      entity.published
+    end
+
+    def text
+      [entity.title, link].join(separator)
+    end
+
+    def attachments
+      [image_url]
+    end
+
+    private
+
+    def image_url
+      Service::Html.first_image_url(entity.content)
+    end
+  end
+end
---
+++ test/normalizers/poorlydrawnlines_normalizer_test.rb
@@ -0,0 +1,57 @@
+require_relative 'normalizer_test'
+
+class PoorlydrawnlinesNormalizerTest < NormalizerTest
+  def sample_data_file
+    'feed_poorlydrawnlines.xml'
+  end
+
+  def processor
+    Processors::FeedjiraProcessor
+  end
+
+  def normalizer
+    Normalizers::PoorlydrawnlinesNormalizer
+  end
+
+  def process_sample_data
+    processor.process(open(sample_data_path).read)
+  end
+
+  def processed
+    @processed ||= process_sample_data
+  end
+
+  def normalize_sample_data
+    processed.map { |entity| normalizer.process(entity[1]) }
+  end
+
+  def normalized
+    @normalized ||= normalize_sample_data
+  end
+
+  def test_sample_data_file_exists
+    assert File.exist?(sample_data_path)
+  end
+
+  def test_have_sample_data
+    assert(processed.present?)
+    assert(processed.length.positive?)
+  end
+
+  def test_normalization
+    assert(normalized.present?)
+    assert(processed.length == normalized.length)
+  end
+
+  FIRST_SAMPLE = {
+    'link' => 'http://www.poorlydrawnlines.com/comic/hello/',
+    'published_at' => Time.parse('2018-10-22 16:03:51 UTC'),
+    'text' => 'Hello - http://www.poorlydrawnlines.com/comic/hello/',
+    'attachments' => ['http://www.poorlydrawnlines.com/wp-content/uploads/2018/10/hello.png'],
+    'comments' => []
+  }.freeze
+
+  def test_normalized_sample
+    assert_equal(FIRST_SAMPLE, normalized.first)
+  end
+end
