f83f3e8773a81d0f32936207c1bdcaffd248d306
Alex Musayev <alex.musayev@gmail.com>
2018-10-26 15:53:02 +0300

    Vagrant provisioning script updated
    - Disable elasticsearch
    - Update Node version from 6 to 8
    - Update Ruby version from 2.4.1 to 2.5.1

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
-export POSTGRES_DB_NAME="passiondig"
+export POSTGRES_DB_NAME="feeder"
 
 set -x
 
@@ -55,40 +55,41 @@ createdb "$POSTGRES_DB_NAME"_production --username=postgres
 
 
 
-echo "-----> install elasticsearch"
+# echo "-----> install elasticsearch"
 
-sudo apt-get install --yes default-jre
-cd
-sudo curl --silent --show-error -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.1/elasticsearch-2.3.1.deb
-sudo dpkg -i elasticsearch-2.3.1.deb
-sudo systemctl enable elasticsearch.service
+# sudo apt-get install --yes default-jre
+# cd
+# sudo curl --silent --show-error -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.1/elasticsearch-2.3.1.deb
+# sudo dpkg -i elasticsearch-2.3.1.deb
+# sudo systemctl enable elasticsearch.service
 
-cd
-sudo rm -rf ./elasticsearch*
+# cd
+# sudo rm -rf ./elasticsearch*
 
-echo "rewrite config"
-sudo bash -c "cat > /etc/elasticsearch/elasticsearch.yml" <<EOL
-index.number_of_shards: 1
-index.number_of_replicas: 0
-network.bind_host: 0
-network.host: 0.0.0.0
-script.inline: on
-script.indexed: on
-http.cors.enabled: true
-http.cors.allow-origin: /https?:\/\/.*/
-EOL
+# echo "rewrite config"
+# sudo bash -c "cat > /etc/elasticsearch/elasticsearch.yml" <<EOL
+# index.number_of_shards: 1
+# index.number_of_replicas: 0
+# network.bind_host: 0
+# network.host: 0.0.0.0
+# script.inline: on
+# script.indexed: on
+# http.cors.enabled: true
+# http.cors.allow-origin: /https?:\/\/.*/
+# EOL
 
-# TODO: Don't add the line if it's already there
-sudo bash -c 'echo "ES_HEAP_SIZE=64m" >> /etc/default/elasticsearch'
+# # TODO: Don't add the line if it's already there
+# sudo bash -c 'echo "ES_HEAP_SIZE=64m" >> /etc/default/elasticsearch'
 
-sudo service elasticsearch start
+# sudo service elasticsearch start
 
 
 
 echo "-----> install nodejs"
 
 cd
-sudo curl --silent --show-error -L https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo curl --silent --show-error -L https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get update
 sudo apt-get install --yes nodejs
 
 
@@ -103,22 +104,28 @@ sudo apt-get update && sudo apt-get install --yes yarn
 
 echo "-----> install ruby"
 
-# Requires software-properties-common
-sudo apt-add-repository --yes ppa:brightbox/ruby-ng
-sudo apt-get update --yes
-sudo apt-get install --yes ruby2.4 ruby2.4-dev
+cd
+rm -rf ~/.rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+rm -rf ~/.rbenv/plugins/ruby-build
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+rm -rf ~/.rbenv/plugins/rbenv-update
+git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
 
-cat >> ~/.bashrc <<EOL
-export GEM_HOME=$HOME/.gem/ruby/2.4.0
-export PATH=$HOME/.gem/ruby/2.4.0/bin:$PATH
-EOL
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
 
-export GEM_HOME=$HOME/.gem/ruby/2.4.0
-export PATH=$HOME/.gem/ruby/2.4.0/bin:$PATH
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+rbenv install 2.5.1
+rbenv global 2.5.1
+rbenv rehash
 
 sudo gem update --system
 gem update
-gem install bundle
+gem install bundler
 
 
 
@@ -140,6 +147,14 @@ echo "---> install ansible"
 sudo apt-add-repository --yes ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install --yes ansible
+
+
+
+echo "---> update .bashrc"
+
+cat >> ~/.bashrc <<EOL
+cd /app
+EOL
 
 
 
