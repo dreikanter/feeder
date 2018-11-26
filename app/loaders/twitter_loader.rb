c53d66c070586ae7c8c7a3013e2e75b7ea75158a
Alex Musayev <alex.musayev@gmail.com>
2018-11-26 19:25:32 +0300

    Twitter loader

--- app/loaders/twitter_loader.rb
+++ app/loaders/twitter_loader.rb
@@ -1,7 +1,24 @@
 module Loaders
   class TwitterLoader < Base
     def call
-      # TODO
+      client.user_timeline(twitter_user)
+    end
+
+    private
+
+    def twitter_user
+      feed.options.fetch('twitter_user')
+    rescue KeyError
+      raise "the feed '#{feed.name}' don't have 'twitter_user' option defined"
+    end
+
+    def client
+      @client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+        config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+        config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+      end
     end
   end
 end
