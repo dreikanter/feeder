2c3a02cb6a2b298638e06e88fd81216c2c613ad1
Alex Musayev <alex.musayev@gmail.com>
2018-11-26 19:25:06 +0300

    HttpLoader should require URL (use NullLoader if there are no URL)

--- app/loaders/http_loader.rb
+++ app/loaders/http_loader.rb
@@ -2,7 +2,8 @@ module Loaders
   class HttpLoader < Base
     def call
       url = feed.url
-      RestClient.get(url).body if url
+      raise "#{self.class.name} requires valid feed url" unless url.present?
+      RestClient.get(url).body
     rescue StandardError => exception
       raise [
         "error fetching feed: '#{feed.name}'",
