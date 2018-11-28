9f3afb7e4e5bc5709ca6932bff80fa2c00552f5d
Alex Musayev <alex.musayev@gmail.com>
2018-11-28 16:00:29 +0300

    Display update batches

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,13 +1,20 @@
 class StatsController < ApplicationController
   def show
-    names = Service::FeedsList.call.map { |feed| feed['name'] }
-    @feeds = Feed.where(name: names).order(:updated_at)
-    @history = recent_posts.group('DATE(created_at)').count.sort.to_h
+    @feeds = feeds
+    @history = history
+    @batches = DataPoint.for('batch').limit(10)
+    @recent_posts = Post.includes(:feed).recent
   end
 
   private
 
-  def recent_posts
-    Post.where('created_at > ?', 30.days.ago)
+  def feeds
+    names = Service::FeedsList.call.map { |feed| feed['name'] }
+    Feed.where(name: names).order(:updated_at)
+  end
+
+  def history
+    posts = Post.where('created_at > ?', 30.days.ago).group('DATE(created_at)')
+    posts.count.sort.to_h
   end
 end
