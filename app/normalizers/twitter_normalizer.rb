a04cb726ae8d2a50e9b84fda61c13112b441531f
Alex Musayev <alex.musayev@gmail.com>
2018-11-29 16:00:15 +0300

    Twitter option: import only posts with attachments #90

--- app/normalizers/twitter_normalizer.rb
+++ app/normalizers/twitter_normalizer.rb
@@ -1,5 +1,13 @@
 module Normalizers
   class TwitterNormalizer < Normalizers::Base
+    def valid?
+      !options['only_with_attachments'] || images.any?
+    end
+
+    def validation_errors
+      'no images'
+    end
+
     def link
       "https://twitter.com/#{user_name}/status/#{entity_id}"
     end
@@ -12,8 +20,11 @@ module Normalizers
       [entity.fetch('text'), "!#{link}"].join(separator)
     end
 
+    def images
+      @images ||= entity.dig('extended_entities', 'media') || []
+    end
+
     def attachments
-      images = entity.dig('extended_entities', 'media') || []
       images.map { |image| image['media_url_https'] }
     end
 
