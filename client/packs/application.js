af20f7db949224683702c5cbb4885d924c40e87e
Alex Musayev <alex.musayev@gmail.com>
2019-05-28 17:15:07 +0300

    Import Bootstrap CSS

--- client/packs/application.js
+++ client/packs/application.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from 'main'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 window.addEventListener('load', () => {
   const element = document.getElementById('root')
