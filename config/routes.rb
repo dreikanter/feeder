6f17d784fe278bb794b0820abfa3adca54a88a18
Alex Musayev <alex.musayev@gmail.com>
2019-05-28 21:26:43 +0300

    Serve static layout for any request that is not API

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'feeds#index'
   resources :feeds, only: %i[index show]
 
   namespace :api, defaults: { format: :json } do
     resources :feeds, only: %i[index show]
   end
+
+  root 'layout#show'
+  get '*path', to: 'layout#show'
 end
