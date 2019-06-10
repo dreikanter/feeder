9ae615095ff067b22d06cdf00b3219c94104364e
Alex Musayev <alex.musayev@gmail.com>
2019-06-10 11:52:52 +0300

    Add posts API routing #129

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :feeds, only: %i[index show] do
       resource :activity, only: :show, controller: :activity
+      resources :posts, only: :index
     end
 
     resource :activity, only: :show, controller: :activity
     resources :batches, only: :index
+    resources :posts, only: :index
   end
 
   root 'layout#show'
