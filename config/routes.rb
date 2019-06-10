7cdb974763a5d9141b994371122e97089eaaa7a8
Alex Musayev <alex.musayev@gmail.com>
2019-06-10 11:55:48 +0300

    Add updates API routing #130

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
     resources :feeds, only: %i[index show] do
       resource :activity, only: :show, controller: :activity
       resources :posts, only: :index
+      resources :updates, only: :index
     end
 
     resource :activity, only: :show, controller: :activity
     resources :batches, only: :index
     resources :posts, only: :index
+    resources :updates, only: :index
   end
 
   root 'layout#show'
