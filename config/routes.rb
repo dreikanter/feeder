39fe8343ba481f7978eb79c579cd514c5f9a7560
Alex Musayev <alex.musayev@gmail.com>
2019-06-10 11:56:27 +0300

    Remove unused feed resources from routing configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :feeds, only: %i[index show]
-
   namespace :api, defaults: { format: :json } do
     resources :feeds, only: %i[index show] do
       resource :activity, only: :show, controller: :activity
