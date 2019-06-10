d021855a8dc627ce838298c5123605970879c340
Alex Musayev <alex.musayev@gmail.com>
2019-06-10 11:49:54 +0300

    Add routing for the posting activity API #126

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :feeds, only: %i[index show]
 
   namespace :api, defaults: { format: :json } do
-    resources :feeds, only: %i[index show]
+    resources :feeds, only: %i[index show] do
+      resource :activity, only: :show, controller: :activity
+    end
+
+    resource :activity, only: :show, controller: :activity
   end
 
   root 'layout#show'
