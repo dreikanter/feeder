33a2969400c972eda4f6415b7287c2dd0d3bc927
Alex Musayev <alex.musayev@gmail.com>
2019-06-13 22:30:35 +0300

    Query update data points #130

--- app/operations/updates/index.rb
+++ app/operations/updates/index.rb
@@ -13,7 +13,13 @@ module Operations
       private
 
       def updates
-        []
+        data_points.map { |dp| dp.details.merge(created_at: dp.created_at) }
+      end
+
+      def data_points
+        scope = DataPoint.recent.for('pull')
+        return scope unless feed_name
+        data_points.where("details->>'feed_name' = ?", feed_name)
       end
 
       def meta
