d09c0a513fe7e08e0a9dc21b436b0a6aa89dfe3b
Alex Musayev <alex.musayev@gmail.com>
2019-06-14 16:53:24 +0300

    Present recent updates index #134

--- client/updates/components/Main/component.js
+++ client/updates/components/Main/component.js
@@ -1,6 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import DataTable from 'lib/components/DataTable'
+import MutedZero from 'lib/components/MutedZero'
 import Pending from 'lib/components/Pending'
+import ReadableTime from 'lib/components/ReadableTime'
+import UpdateStatus from 'updates/components/UpdateStatus'
+
+/* eslint-disable react/prop-types, camelcase */
+const cols = [
+  {
+    title: 'Feed name',
+    classes: '',
+    value: ({ feed_name, feed_url }) => (
+      <a href={feed_url}>{feed_name}</a>
+    )
+  },
+  {
+    title: 'Status',
+    classes: '',
+    value: ({ status }) => (
+      <UpdateStatus value={status} />
+    )
+  },
+  {
+    title: 'New posts',
+    classes: '',
+    value: ({ posts_count }) => (
+      <MutedZero value={posts_count} />
+    )
+  },
+  {
+    title: 'Executed at',
+    classes: '',
+    value: ({ started_at }) => (
+      <ReadableTime value={started_at} />
+    )
+  },
+  {
+    title: 'Duration',
+    classes: '',
+    value: ({ duration }) => (
+      duration.toPrecision(2)
+    )
+  },
+]
+/* eslint-enable react/prop-types, camelcase */
 
 class Main extends Component {
   componentDidMount () {
@@ -18,9 +64,13 @@ class Main extends Component {
     }
 
     return (
-      <pre>
-        {JSON.stringify(updates, null, 2)}
-      </pre>
+      <Fragment>
+        <h1>Recent posts</h1>
+        <DataTable
+          cols={cols}
+          records={updates}
+        />
+      </Fragment>
     )
   }
 }
---
+++ client/updates/components/UpdateStatus/component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function UpdateStatus ({ value }) {
+
+}
+
+UpdateStatus.propTypes = {
+  value: PropTypes.string.isRequired
+}
+
+export default UpdateStatus
