2e01fe2a2f1da5dbeffacf73d8aab153c45b3315
Alex Musayev <alex.musayev@gmail.com>
2019-06-14 16:18:48 +0300

    Improve feeds index layout

--- client/index/components/Feeds/component.js
+++ client/index/components/Feeds/component.js
@@ -5,33 +5,32 @@ import DataTable from 'lib/components/DataTable'
 import ago from 'lib/utils/ago'
 import paths from 'main/paths'
 
+/* eslint-disable react/prop-types, camelcase */
 const cols = [
   {
     title: 'Feed name',
-    classes: '',
+    classes: 'col-auto text-nowrap',
     value: ({ id, name }) => (
       <Link to={paths.feedPath(id)}>{name}</Link>
     )
   },
   {
     title: 'Posts',
-    classes: 'min text-center',
-    // eslint-disable-next-line
+    classes: 'col-auto text-center text-nowrap',
     value: ({ posts_count }) => posts_count
   },
   {
     title: 'Last post',
-    classes: '',
-    // eslint-disable-next-line
+    classes: 'col-auto text-nowrap',
     value: ({ last_post_created_at }) => ago(last_post_created_at)
   },
   {
     title: 'Refreshed at',
-    classes: '',
-    // eslint-disable-next-line
+    classes: 'col-auto text-nowrap',
     value: ({ refreshed_at }) => ago(refreshed_at)
   },
 ]
+/* eslint-enable react/prop-types, camelcase */
 
 const Feeds = ({ click, records }) => (
   <DataTable
