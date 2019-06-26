77423be3444b6d9fd5676b182a90a05123cc271b
Alex Musayev <alex.musayev@gmail.com>
2019-06-26 16:29:59 +0300

    Add className prop to Stats

--- client/lib/components/Stats/component.js
+++ client/lib/components/Stats/component.js
@@ -9,8 +9,8 @@ const valueClasses = value => cc([
   { 'Stats__value--empty': !value }
 ])
 
-const Stats = ({ items, placeholder }) => (
-  <div className="card-group mb-3 Stats">
+const Stats = ({ className, items, placeholder }) => (
+  <div className={cc(['card-group', 'mb-3', 'Stats', className])}>
     {items.map(({ title, value }, index) => (
       <div className="card" key={index}>
         <div className="card-body">
@@ -23,6 +23,7 @@ const Stats = ({ items, placeholder }) => (
 )
 
 Stats.propTypes = {
+  className: PropTypes.string,
   items: PropTypes.arrayOf(PropTypes.shape({
     title: PropTypes.string,
     value: PropTypes.any
@@ -31,6 +32,7 @@ Stats.propTypes = {
 }
 
 Stats.defaultProps = {
+  className: undefined,
   items: [],
   placeholder: <Mute>–</Mute>
 }
