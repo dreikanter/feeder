117aaaa2112e36c817c1157c2405616cc504a5a9
Alex Musayev <alex.musayev@gmail.com>
2019-07-05 17:13:10 +0300

    Improve validation logic

--- app/normalizers/twitter_normalizer.rb
+++ app/normalizers/twitter_normalizer.rb
@@ -1,13 +1,14 @@
 module Normalizers
   class TwitterNormalizer < Normalizers::Base
     def valid?
-      return false if options['only_with_attachments'] && !has_attachments?
-      return false if options['ignore_retweets'] && retweet?
-      true
+      !(violates_no_images? && violates_retweet?)
     end
 
     def validation_errors
-      'no images'
+      Array.new.tap do |errors|
+        errors << 'no images' if violates_no_images?
+        errors << 'retweet' if violates_retweet?
+      end
     end
 
     def link
@@ -22,13 +23,11 @@ module Normalizers
       [entity.fetch('text'), "!#{link}"].join(separator)
     end
 
-    def images
-      @images ||= entity.dig('extended_entities', 'media') || []
+    def attachments
+      images.map { |image| image['media_url_https'] }.reject(&:blank?)
     end
 
-    def attachments
-      images.map { |image| image['media_url_https'] }
-    end
+    private
 
     def entity_id
       entity.fetch('id')
@@ -38,12 +37,24 @@ module Normalizers
       entity.fetch('user').fetch('screen_name')
     end
 
-    def has_attachments?
-      images.any?
+    def images
+      @images ||= entity.dig('extended_entities', 'media') || []
+    end
+
+    def no_images?
+      images.empty?
     end
 
     def retweet?
       entity['retweeted_status'].present?
+    end
+
+    def violates_no_images?
+      options['only_with_attachments'] && no_images?
+    end
+
+    def violates_retweet?
+      options['ignore_retweets'] && retweet?
     end
   end
 end
