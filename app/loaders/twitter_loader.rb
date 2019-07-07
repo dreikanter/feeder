7298e3cbefc022a9ee3ae9d0fd86f5b1a4c1b5f7
Alex Musayev <alex.musayev@gmail.com>
2019-07-07 20:06:50 +0300

    Validate TwitterLoader options #161

--- app/loaders/twitter_loader.rb
+++ app/loaders/twitter_loader.rb
@@ -1,31 +1,52 @@
+# TwitterLoader requires "twitter" block in the application credentials file
+# to contain Twitter API access credentials. See REQUIRED_OPTIONS definition
+# and 'twitter' gem documentation.
+#
+# Rails.application.credentials.twitter will be used unless loader options
+# are defined during the loader instance initialization.
+#
+# SEE: https://www.rubydoc.info/gems/twitter
+
 module Loaders
   class TwitterLoader < Base
+    REQUIRED_OPTIONS = %i[
+      consumer_key
+      consumer_secret
+      access_token
+      access_token_secret
+    ].freeze
+
     def call
+      validate_options!
       client.user_timeline(twitter_user)
     end
 
     private
 
-    def twitter_user
-      feed.options.fetch('twitter_user')
-    rescue KeyError
-      raise "the feed '#{feed.name}' don't have 'twitter_user' option defined"
+    def validate_options!
+      options_or_defaults.each do |option_name|
+        next if options_or_defaults[option_name].present?
+        raise "required option '#{option_name}' is not defined"
+      end
     end
 
     def client
-      @client ||= Twitter::REST::Client.new do |config|
-        config.consumer_key =
-          Rails.application.credentials.twitter_consumer_key
+      Twitter::REST::Client.new(safe_options)
+    end
 
-        config.consumer_secret =
-          Rails.application.credentials.twitter_consumer_secret
+    def twitter_user
+      feed.options.fetch('twitter_user')
+    rescue KeyError
+      raise "'twitter_user' option is not defined for '#{feed.name}' feed"
+    end
 
-        config.access_token =
-          Rails.application.credentials.twitter_access_token
+    def options_or_defaults
+      return options unless options.empty?
+      Rails.application.credentials.twitter
+    end
 
-        config.access_token_secret =
-          Rails.application.credentials.twitter_access_token_secret
-      end
+    def safe_options
+      options_or_defaults.slice(*REQUIRED_OPTIONS)
     end
   end
 end
