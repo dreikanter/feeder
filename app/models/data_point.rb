12cd1470db477b901d5183d92ed85ada5108ff9c
Alex Musayev <alex.musayev@gmail.com>
2019-07-09 17:28:05 +0300

    Implement respond_to_missing? companion method for method_missing?
    See https://thoughtbot.com/blog/always-define-respond-to-missing-when-overriding

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -19,16 +19,23 @@ class DataPoint < ApplicationRecord
   RECENT_LIMIT = 50
   KEEP_RECORDS_FOR = 3.month
 
+  CREATE_METHOD_PREFIX = 'create_'.freeze
+
   scope :ordered, -> { order(created_at: :desc) }
   scope :recent, -> { ordered.limit(RECENT_LIMIT) }
 
   def self.method_missing(method_name, details = {})
-    return super unless method_name.to_s.starts_with?('create_')
+    return super unless valid_create_method_name?(method_name)
+
     series = method_name.to_s.sub(/^create_/, '')
     DataPoint.create(
       series: DataPointSeries.find_or_create_by(name: series),
       details: details
     )
+  end
+
+  def respond_to_missing?(method_name)
+    valid_create_method_name?(method_name) || super
   end
 
   def self.for(series_name)
@@ -37,5 +44,11 @@ class DataPoint < ApplicationRecord
 
   def self.purge_old_records!
     DataPoint.where('created_at < ?', KEEP_RECORDS_FOR.ago).delete_all
+  end
+
+  private
+
+  def valid_create_method_name?(method_name)
+    method_name.to_s.starts_with?(CREATE_METHOD_PREFIX)
   end
 end
