41f95ec1ed5b3d200b30b77db0e95f7f82785307
Alex Musayev <alex.musayev@gmail.com>
2019-07-09 16:50:00 +0300

    Drop redundant curly braces

--- test/routing/layout_routing_test.rb
+++ test/routing/layout_routing_test.rb
@@ -12,28 +12,28 @@ class LayoutRoutingTest < ActionDispatch::IntegrationTest
     }
   end
 
-
   def test_feeds_index
     path = feeds_path
-    assert_recognizes(expected_options, { path: path, method: :get })
+    assert_recognizes(expected_options, path: path, method: :get)
   end
 
   def test_feed_show
     path = feed_path(SAMPLE_FEED_NAME)
-    assert_recognizes(expected_options.merge(name: SAMPLE_FEED_NAME), {
+    assert_recognizes(
+      expected_options.merge(name: SAMPLE_FEED_NAME),
       path: path,
       method: :get,
       name: SAMPLE_FEED_NAME
-    })
+    )
   end
 
   def test_updates_index
     path = updates_path
-    assert_recognizes(expected_options, { path: path, method: :get })
+    assert_recognizes(expected_options, path: path, method: :get)
   end
 
   def test_posts_index
     path = posts_path
-    assert_recognizes(expected_options, { path: path, method: :get })
+    assert_recognizes(expected_options, path: path, method: :get)
   end
 end
