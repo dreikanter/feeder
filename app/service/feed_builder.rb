95172aeafacdff257adb30d6ef4e5e317d1a6599
Alex Musayev <alex.musayev@gmail.com>
2019-07-29 22:56:52 +0300

    Decouple FeedBuilder from FeedsList

--- app/service/feed_builder.rb
+++ app/service/feed_builder.rb
@@ -1,12 +1,12 @@
 module Service
   class FeedBuilder
-    DEFAULT_CONF_FETCHER = ->(feed_name) { Service::FeedsList[feed_name] }
+    DEFAULT_CONFIG = Service::FeedsList
 
-    def self.call(feed_name, conf_fetcher = DEFAULT_CONF_FETCHER)
-      options = conf_fetcher.call(feed_name)
-      raise 'feed configuration not found' unless options
-      feed = Feed.find_or_create_by(name: options[:name])
-      feed.update({ status: Enums::FeedStatus.active }.merge(options))
+    def self.call(feed_name, config = DEFAULT_CONFIG)
+      feed_config = config[feed_name]
+      raise 'feed configuration not found' unless feed_config
+      feed = Feed.find_or_create_by(name: feed_name)
+      feed.update({ status: Enums::FeedStatus.active }.merge(feed_config))
       feed
     end
   end
--- test/service/feed_builder_test.rb
+++ test/service/feed_builder_test.rb
@@ -5,20 +5,36 @@ class FeedBuilderTest < Minitest::Test
     Service::FeedBuilder
   end
 
-  SAMPLE_CONFIG_PATH =
-    File.expand_path(File.join(File.dirname(__FILE__), './feeds.yml')).freeze
+  CONFIG = [
+    {
+      name: 'xkcd',
+      url: 'http://xkcd.com/rss.xml',
+      processor: 'rss',
+      after: '2018-09-15T00:00:00+00:00'
+    },
+    {
+      name: 'oglaf',
+      url: 'http://oglaf.com/feeds/rss/',
+      processor: 'oglaf',
+      normalizer: 'oglaf',
+      after: '2018-09-18T00:00:00+00:00'
+    },
+    {
+      name: 'phdcomics',
+      url: 'http://feeds.feedburner.com/PhdComics',
+      processor: 'rss',
+      after: '2018-09-18T00:00:00+00:00'
+    }
+  ].freeze
 
-  def test_sample_config_exists
-    message = "#{SAMPLE_CONFIG_PATH} should exist"
-    assert(File.exist?(SAMPLE_CONFIG_PATH), message)
+  def setup
+    Feed.delete_all
   end
 
   def test_find
-    feeds = Service::FeedsList.call(SAMPLE_CONFIG_PATH)
-    feeds.each do |feed|
-      feed_name = feed[:name]
-      conf = ->(feed_name) { feeds.find { |feed| feed[:name] == feed_name } }
-      result = service.call(feed_name, conf)
+    CONFIG.map { |feed| feed[:name] }.each do |feed_name|
+      fetcher = ->(name) { CONFIG.find { |feed| feed[:name] == name } }
+      result = service.call(feed_name, fetcher)
       expected = Feed.find_by(name: feed_name)
       assert_equal(result, expected)
     end
