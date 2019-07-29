86def860540cc1ad8202d80b293a5006620d28c2
Alex Musayev <alex.musayev@gmail.com>
2019-07-30 00:22:54 +0300

    Bypass batch data point to PullJob #171

--- app/jobs/batch_pull_job.rb
+++ app/jobs/batch_pull_job.rb
@@ -2,16 +2,10 @@ class BatchPullJob < ApplicationJob
   queue_as :default
 
   def perform
-    started_at = Time.now.utc
+    batch = Service::CreateDataPoint.call(:batch, started_at: Time.now.utc)
     names = Service::FeedsList.names
     Feed.where(name: names).update_all(status: Enums::FeedStatus.active)
     Feed.where.not(name: names).update_all(status: Enums::FeedStatus.inactive)
-    names.each { |feed_name| PullJob.perform_later(feed_name) }
-
-    Service::CreateDataPoint.call(
-      :batch,
-      started_at: started_at,
-      duration: Time.new.utc - started_at
-    )
+    names.each { |feed_name| PullJob.perform_later(feed_name, batch) }
   end
 end
