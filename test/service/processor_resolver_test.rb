35e1c5abea5c5690baf8e4cb7cc8bc26c5b11f30
Alex Musayev <alex.musayev@gmail.com>
2019-07-29 18:52:37 +0300

    Refactor ProcessorResolver test

--- test/service/processor_resolver_test.rb
+++ test/service/processor_resolver_test.rb
@@ -3,29 +3,31 @@ require 'test_helper'
 class ProcessorResolverTest < Minitest::Test
   UNRESOLVABLE = 'unresolvable'.freeze
 
-  def assert_resolve(expected, attributes)
-    feed = Feed.new(attributes)
-    result = Service::ProcessorResolver.call(feed)
-    assert_equal(expected, result)
+  def service
+    Service::ProcessorResolver
+  end
+
+  def test_callable
+    assert(service.respond_to?(:call))
   end
 
   def test_explicit_resolution_for_rss
-    attributes = { name: 'xkcd', processor: 'rss' }
-    assert_resolve(Processors::RssProcessor, attributes)
+    feed = build(:feed, name: 'xkcd', processor: 'rss')
+    assert_equal(Processors::RssProcessor, service.call(feed))
   end
 
   def test_explicit_resolution_for_atom
-    attributes = { name: 'dilbert', processor: 'atom' }
-    assert_resolve(Processors::AtomProcessor, attributes)
+    feed = build(:feed, name: 'dilbert', processor: 'atom')
+    assert_equal(Processors::AtomProcessor, service.call(feed))
   end
 
   def test_resolution_by_name
-    attributes = { name: 'atom', processor: 'unresolvable' }
-    assert_resolve(Processors::AtomProcessor, attributes)
+    feed = build(:feed, name: 'atom', processor: 'unresolvable')
+    assert_equal(Processors::AtomProcessor, service.call(feed))
   end
 
   def test_fallback
-    attributes = { name: 'unresolvable', processor: 'unresolvable' }
-    assert_resolve(Processors::NullProcessor, attributes)
+    feed = build(:feed, name: 'unresolvable', processor: 'unresolvable')
+    assert_equal(Processors::NullProcessor, service.call(feed))
   end
 end
