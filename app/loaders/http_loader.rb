94e3789b19f343ffc38d9f1c929229b20544e059
Alex Musayev <alex.musayev@gmail.com>
2019-08-06 21:50:15 +0300

    Remove redundant exception wrapper from HttpLoader

--- app/loaders/http_loader.rb
+++ app/loaders/http_loader.rb
@@ -3,15 +3,7 @@ module Loaders
     def call
       url = feed.url
       raise "#{self.class.name} requires valid feed url" unless url.present?
-
       RestClient.get(url).body
-    rescue StandardError => e
-      raise [
-        "error fetching feed: '#{feed.name}'",
-        "url: '#{url}'",
-        e.class.name,
-        e.message
-      ].join('; ')
     end
   end
 end
