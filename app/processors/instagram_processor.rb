cbd1c21495c96c757886a2c37510b8dff4bccaa6
Alex Musayev <alex.musayev@gmail.com>
2019-08-15 15:32:18 +0300

    Instagram processor #91

---
+++ app/processors/instagram_processor.rb
@@ -0,0 +1,27 @@
+module Processors
+  class InstagramProcessor < Processors::Base
+    protected
+
+    def entities
+      nodes.map { |node| [node['id'], node] }.to_h
+    end
+
+    EDGES_PATH = [
+      'entry_data',
+      'ProfilePage',
+      0,
+      'graphql',
+      'user',
+      'edge_owner_to_timeline_media',
+      'edges'
+    ].freeze
+
+    def edges
+      source.dig(*EDGES_PATH)
+    end
+
+    def nodes
+      edges.map { |edge| edge['node'] }
+    end
+  end
+end
---
+++ test/processors/instagram_processor_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+require_relative 'processor_test_helpers'
+
+module Processors
+  class InstagramProcessorTest < Minitest::Test
+    include ProcessorTestHelpers
+
+    def sample_data_file
+      'feed_instagram.json'
+    end
+
+    def subject
+      Processors::InstagramProcessor
+    end
+
+    def sample_json
+      JSON.parse(sample_data)
+    end
+
+    def result
+      @result ||= subject.call(sample_json, create(:feed))
+    end
+
+    def test_success
+      assert(result.success?)
+    end
+
+    def test_array
+      value = result.value!
+      assert(value.is_a?(Array))
+    end
+
+    def test_ids
+      result.value!.each do |entity|
+        expected_id = entity[1]['id']
+        assert(expected_id)
+        assert(entity[0])
+        assert_equal(expected_id, entity[0])
+      end
+    end
+  end
+end
