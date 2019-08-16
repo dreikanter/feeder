d3f128593fcc78e8c32ca4ff1935e3d80f5486c1
Alex Musayev <alex.musayev@gmail.com>
2019-08-16 22:13:13 +0300

    Import infiniteimmortalbens #180

---
+++ test/normalizers/infiniteimmortalbens_normalizer_test.rb
@@ -0,0 +1,56 @@
+require 'test_helper'
+require_relative '../support/normalizer_test_helper'
+
+class InfiniteimmortalbensNormalizerTest < Minitest::Test
+  include NormalizerTestHelper
+
+  def subject
+    Normalizers::InfiniteimmortalbensNormalizer
+  end
+
+  def processor
+    Processors::RssProcessor
+  end
+
+  def sample_data_file
+    'feed_infiniteimmortalbens.xml'.freeze
+  end
+
+  def test_success
+    normalized.each do |entity|
+      assert(entity.success?)
+    end
+  end
+
+  def test_text
+    normalized.each do |entity|
+      refute(entity.value!['text'].empty?)
+    end
+  end
+
+  def test_comments
+    normalized.each do |entity|
+      assert(entity.value!['comments'].empty?)
+    end
+  end
+
+  def test_valid_link
+    normalized.each do |entity|
+      Addressable::URI.parse(entity.value!['link'])
+    end
+  end
+
+  def test_published_at
+    normalized.each do |entity|
+      assert(entity.value!['published_at'].is_a?(Time))
+    end
+  end
+
+  def test_attachments
+    normalized.each do |entity|
+      result = entity.value!['attachments']
+      assert(result.is_a?(Array))
+      assert(result.any?)
+    end
+  end
+end
