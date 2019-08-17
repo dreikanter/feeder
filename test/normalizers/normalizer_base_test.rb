0dc5aa1801d704ab28ce9edb33fc48eb97f9c961
Alex Musayev <alex.musayev@gmail.com>
2019-08-17 14:16:17 +0300

    Sanitize attachment URLs; do not tolerate non valid #attachments value

--- test/normalizers/normalizer_base_test.rb
+++ test/normalizers/normalizer_base_test.rb
@@ -37,16 +37,73 @@ class NormalizerBaseTest < Minitest::Test
     uid
   ].to_set.freeze
 
-  def test_return_attributes_hash
-    result = Class.new(subject).call(uid, ENTITY, feed).value!
-    assert_equal(EXPECTED_ATTRIBUTES, result.keys.to_set)
+  def test_minimal_viable_inheritence
+    normalizer = Class.new(subject)
+    result = normalizer.call(uid, ENTITY, feed)
+    assert(result.success?)
   end
 
-  def test_failure_normalizer
+  def test_validation_errors
     normalizer = Class.new(subject) do
       define_method('validation_errors') { ['sample error'] }
     end
     result = normalizer.call(uid, ENTITY, feed)
     assert(result.failure?)
+  end
+
+  def test_return_hash
+    result = Class.new(subject).call(uid, ENTITY, feed).value!
+    assert_equal(EXPECTED_ATTRIBUTES, result.keys.to_set)
+  end
+
+  def test_sanitize_attachments
+    incomplete_url = '//example.com'
+    normalizer = Class.new(subject) do
+      define_method('attachments') { [incomplete_url] }
+    end
+    result = normalizer.call(uid, ENTITY, feed)
+    expected = ["https:#{incomplete_url}"]
+    assert(expected, result.value!['attachments'])
+  end
+
+  def test_require_valid_attachment_urls
+    non_valid_url = ':'
+    normalizer = Class.new(subject) do
+      define_method('attachments') { [non_valid_url] }
+    end
+    result = normalizer.call(uid, ENTITY, feed)
+    assert(result.failure?)
+  end
+
+  def test_require_attachments_array
+    normalizer = Class.new(subject) do
+      define_method('attachments') { nil }
+    end
+    result = normalizer.call(uid, ENTITY, feed)
+    assert(result.failure?)
+  end
+
+  def test_drop_empty_attachment_urls
+    normalizer = Class.new(subject) do
+      define_method('attachments') { [nil, ''] }
+    end
+    result = normalizer.call(uid, ENTITY, feed)
+    assert(result.value!['attachments'].empty?)
+  end
+
+  def test_require_comments_array
+    normalizer = Class.new(subject) do
+      define_method('comments') { nil }
+    end
+    result = normalizer.call(uid, ENTITY, feed)
+    assert(result.failure?)
+  end
+
+  def test_drop_empty_comments
+    normalizer = Class.new(subject) do
+      define_method('comments') { [nil, ''] }
+    end
+    result = normalizer.call(uid, ENTITY, feed)
+    assert(result.value!['comments'].empty?)
   end
 end
