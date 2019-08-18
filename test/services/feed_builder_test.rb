1fb7cc2603387bfdac386e75890506c7997ba6a8
Alex Musayev <alex.musayev@gmail.com>
2019-08-18 20:59:09 +0300

    Structural refactoring to match Zeitwerk autoloader rules #184
    - Remove constants module
    - Remove redundant namespaces
    - Update base class names to prevent resolution collisions

---
+++ test/services/feed_builder_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class FeedBuilderTest < Minitest::Test
+  def subject
+    FeedBuilder
+  end
+
+  CONFIG = [
+    {
+      name: 'xkcd',
+      url: 'http://xkcd.com/rss.xml',
+      processor: 'rss',
+      after: '2018-09-15T00:00:00+00:00'
+    },
+    {
+      name: 'oglaf',
+      url: 'http://oglaf.com/feeds/rss/',
+      processor: 'oglaf',
+      normalizer: 'oglaf',
+      after: '2018-09-18T00:00:00+00:00'
+    }
+  ].freeze
+
+  def setup
+    Feed.delete_all
+    CONFIG.each { |feed| Feed.create!(**feed) }
+  end
+
+  def test_find
+    CONFIG.map { |feed| feed[:name] }.each do |feed_name|
+      feeds_list = Feed.all
+      result = subject.call(feed_name, feeds_list: feeds_list)
+      expected = Feed.find_by(name: feed_name)
+      assert_equal(result, expected)
+    end
+  end
+
+  def test_raise_when_feed_name_is_not_found
+    not_existing_feed_name = '404'
+    assert_raises(StandardError) { subject.call(not_existing_feed_name) }
+  end
+end
--- test/services/pull_test.rb
+++ test/services/pull_test.rb
@@ -9,73 +9,32 @@ class PullTest < Minitest::Test
   end
 
   def feed
-    build(:feed, name: :test, import_limit: 0)
+    create(:feed, name: :test, import_limit: 0)
+  end
+
+  def test_loader
+    result = LoaderResolver.call(feed)
+    assert_equal(TestLoader, result)
+  end
+
+  def test_processor
+    result = ProcessorResolver.call(feed)
+    assert_equal(TestProcessor, result)
+  end
+
+  def test_normalizer
+    result = NormalizerResolver.call(feed)
+    assert_equal(TestNormalizer, result)
   end
 
   def test_requires_feed_param
     assert_raises(ArgumentError) { subject.call }
   end
 
-  def test_resolve_support_loader
-    result = LoaderResolver.call(feed)
-    assert_equal(TestLoader, result)
-  end
-
-  def test_resolve_support_processor
-    result = ProcessorResolver.call(feed)
-    assert_equal(TestProcessor, result)
-  end
-
-  def test_resolve_support_normalizer
-    result = NormalizerResolver.call(feed)
-    assert_equal(TestNormalizer, result)
-  end
-
-  def test_handle_loader_error
-    loader = Class.new(BaseLoader) { define_method(:perform) { raise } }
-    result = subject.call(feed, loader: loader)
-    assert(result.failure?)
-  end
-
-  def test_handle_processor_error
-    processor = Class.new(BaseProcessor) { define_method(:entities) { raise } }
-    result = subject.call(feed, processor: processor)
-    assert(result.failure?)
-  end
-
-  def test_handle_normalization_error
-    norm = Class.new(BaseNormalizer) { define_method(:link) { raise } }
-    result = subject.call(feed, normalizer: norm)
-    assert(result.success?)
-    assert(result.value!.all?(&:failure?))
-  end
-
-  SOME_ENTITIES = [[Object.new, Object.new]].freeze
-
-  def test_handle_normalizer_resolution_error
-    feed = build(:feed, name: :unresolvable)
-    assert_raises(RuntimeError) { NormalizerResolver.call(feed) }
-    processor = Class.new(BaseProcessor) do
-      define_method(:entities) { SOME_ENTITIES }
-    end
-    result = subject.call(feed, processor: processor)
-    assert(result.failure?)
-  end
-
-  SOME_ERRORS = ['some errors'].freeze
-
-  def test_handle_validation_error
-    norm = Class.new(BaseNormalizer) do
-      define_method(:validation_errors) { SOME_ERRORS }
-    end
-    result = subject.call(feed, normalizer: norm)
-    assert(result.success?)
-    assert(result.value!.all?(&:failure?))
-  end
-
-  def test_happy_path
-    result = subject.call(feed)
-    assert(result.success?)
-    assert(result.value!.all?(&:success?))
+  def test_call
+    expected_entities_count = TestProcessor::ENTITIES.count
+    posts = subject.call(feed)
+    assert(posts.is_a?(Enumerable))
+    assert_equal(expected_entities_count, posts.count)
   end
 end
