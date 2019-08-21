6ae8ae827ee4cd621913faafde4ab6bbf35baabe
Alex Musayev <alex.musayev@gmail.com>
2019-08-21 23:32:54 +0300

    Fix log message

--- app/jobs/batch_pull_job.rb
+++ app/jobs/batch_pull_job.rb
@@ -2,7 +2,7 @@ class BatchPullJob < ApplicationJob
   queue_as :default
 
   def perform
-    logger.info("batch update: #{stale_feeds.count} feed(s)")
+    logger.info("batch update: #{Feed.stale.count} feed(s)")
     Feed.stale.each do |feed|
       PullJob.perform_later(feed)
     end
