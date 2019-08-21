b247ae6238db42bd15d7d6ff34a21188d8c5dc42
Alex Musayev <alex.musayev@gmail.com>
2019-08-21 23:33:43 +0300

    Fix autoload warning message

--- config/initializers/update_feeds.rb
+++ config/initializers/update_feeds.rb
@@ -1,1 +1,8 @@
+# TODO: Consider calling UpdateFeeds from elsewhere
+require_relative '../../app/enums/feed_status'
+require_relative '../../app/models/application_record'
+require_relative '../../app/models/feed'
+require_relative '../../app/services/feed_sanitizer'
+require_relative '../../app/services/update_feeds'
+
 UpdateFeeds.call
