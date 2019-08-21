7c3b85c60aa89b29a80639632f2dc02a9403ad1b
Alex Musayev <alex.musayev@gmail.com>
2019-08-21 15:24:48 +0300

    Update only the allowed feed atributes #174 #188

--- app/services/update_feeds.rb
+++ app/services/update_feeds.rb
@@ -6,6 +6,18 @@ class UpdateFeeds
   option :path, optional: true, default: -> { DEFAULT_PATH }
   option :logger, optional: true, default: -> { Rails.logger }
 
+  ALLOWED_ATTRIBUTES = %i[
+    after
+    import_limit
+    loader
+    name
+    normalizer
+    options
+    processor
+    refresh_interval
+    url
+  ].freeze
+
   def call
     logger.info('updating feeds from configuration')
     update_active_feeds
@@ -15,7 +27,8 @@ class UpdateFeeds
   def update_active_feeds
     feeds.each do |config|
       feed = Feed.find_or_create_by(name: config[:name])
-      feed.update(config.merge(status: FeedStatus.active))
+      safe_config = config.slice(*ALLOWED_ATTRIBUTES)
+      feed.update(safe_config.merge(status: FeedStatus.active))
     end
   end
 
