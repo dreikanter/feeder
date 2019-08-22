439bf197090c6aa5b30de263cf1d41ea8bdcdb03
Alex Musayev <alex.musayev@gmail.com>
2019-08-22 21:20:54 +0300

    Create data point during BatchPullJob

--- app/jobs/batch_pull_job.rb
+++ app/jobs/batch_pull_job.rb
@@ -2,8 +2,10 @@ class BatchPullJob < ApplicationJob
   queue_as :default
 
   def perform
-    logger.info("batch update: #{Feed.stale.count} feed(s)")
-    Feed.stale.each do |feed|
+    stale_feeds = Feed.stale
+    logger.info("batch update: #{stale_feeds.count} feed(s)")
+    CreateDataPoint.call(:batch, feeds: stale_feeds.pluck(:name))
+    stale_feeds.each do |feed|
       PullJob.perform_later(feed)
     end
   end
