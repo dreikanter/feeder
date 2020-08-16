d6fea162db205a5862b9101e695427a21b543151
Alex Musayev <alex.musayev@gmail.com>
2020-08-16 14:18:32 +0300

    Fix commitstrip

--- app/normalizers/commitstrip_normalizer.rb
+++ app/normalizers/commitstrip_normalizer.rb
@@ -2,11 +2,11 @@ class CommitstripNormalizer < FeedjiraNormalizer
   protected
 
   def link
-    entity.link
+    entity.url
   end
 
   def attachments
-    [Nokogiri::HTML(entity.content_encoded).css('img:first').first['src']]
+    [Nokogiri::HTML(entity.content).css('img:first').first['src']]
   end
 
   def comments
