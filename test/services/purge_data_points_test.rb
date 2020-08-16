0978a7c9bff65e4d42e2812eb534f409073ea46b
Alex Musayev <alex.musayev@gmail.com>
2020-08-16 14:18:50 +0300

    Fix PurgeDataPoints test

--- test/services/purge_data_points_test.rb
+++ test/services/purge_data_points_test.rb
@@ -1,21 +1,28 @@
 require 'test_helper'
 
+# TODO: Freeze time
 class PurgeDataPointsTest < Minitest::Test
   def subject
     PurgeDataPoints
   end
 
-  SAMPLE_THRESHOLD = Time.now.utc.freeze
+  SAMPLE_THRESHOLD = 1
 
   def test_deletes_old_data_points
-    data_point = create(:data_point, created_at: SAMPLE_THRESHOLD - 1)
+    data_point = create(:data_point, created_at: sample_threshold - 1)
     subject.call(threshold: SAMPLE_THRESHOLD)
     refute(DataPoint.exists?(data_point.id))
   end
 
   def test_keep_recent_data_points
-    data_point = create(:data_point, created_at: SAMPLE_THRESHOLD + 1)
+    data_point = create(:data_point, created_at: sample_threshold + 1)
     subject.call(threshold: SAMPLE_THRESHOLD)
     assert(DataPoint.exists?(data_point.id))
+  end
+
+  private
+
+  def sample_threshold
+    SAMPLE_THRESHOLD.months.ago
   end
 end
