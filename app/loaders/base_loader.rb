9d496f409b286520663233f18d8e2a92886be5f5
Alex Musayev <alex.musayev@gmail.com>
2020-10-04 18:02:05 +0300

    Remove Dry::Monads from BaseLoader

--- app/loaders/base_loader.rb
+++ app/loaders/base_loader.rb
@@ -1,16 +1,12 @@
 class BaseLoader
   include Callee
-  include Dry::Monads[:result]
 
   param :feed
   option :logger, optional: true, default: -> { Rails.logger }
 
   def call
     logger.info("loading feed [#{feed&.name}]")
-    Success(perform)
-  rescue StandardError => e
-    logger.error("error loading feed [#{feed&.name}]")
-    Failure(e)
+    perform
   end
 
   protected
