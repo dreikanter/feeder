da4971d7383fcc513f84b2c669cf86001482ed7e
Alex Musayev <alex.musayev@gmail.com>
2020-10-08 00:56:19 +0300

    Logging readability

--- app/jobs/batch_pull_job.rb
+++ app/jobs/batch_pull_job.rb
@@ -3,8 +3,9 @@ class BatchPullJob < ApplicationJob
 
   def perform
     stale_feeds = Feed.stale
-    logger.info("batch update: #{stale_feeds.count} feed(s)")
+    logger.info("---> batch update: #{stale_feeds.count} feed(s)")
     CreateDataPoint.call(:batch, feeds: stale_feeds.pluck(:name))
+
     stale_feeds.each do |feed|
       # TODO: Exclude inactive feeds from stale scope
       PullJob.perform_later(feed) if feed.active?
