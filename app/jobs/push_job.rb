76d012342992fadd4bff2e6185dd2b4014db52fa
Alex Musayev <alex.musayev@gmail.com>
2020-10-11 00:22:13 +0300

    Test PushJob

--- app/jobs/push_job.rb
+++ app/jobs/push_job.rb
@@ -2,16 +2,6 @@ class PushJob < ApplicationJob
   queue_as :default
 
   def perform(post)
-    if post.ignored?
-      logger.info('ignoring non-valid post')
-      return
-    end
-
-    unless post.ready?
-      logger.error("unexpected post status: #{post.status}")
-      return
-    end
-
     Push.call(post)
   end
 end
