b62b25765c58a93bff000e0708b73905cbdebae0
Alex Musayev <alex.musayev@gmail.com>
2020-11-09 12:12:46 +0300

    Replace redundant method

--- test/feeds/buni_test.rb
+++ test/feeds/buni_test.rb
@@ -17,19 +17,14 @@ class BuniTest < Minitest::Test
     }
   end
 
-  # TODO
-  def sample_file(path)
-    File.read(File.join(File.expand_path('../data', __dir__).freeze, path))
-  end
-
   def setup
     super
 
-    webtoons_post = sample_file('post_buni_webtoons.html')
+    webtoons_post = file_fixture('posts/buni_webtoons.html').read
     stub_request(:get, 'http://www.bunicomic.com/2019/11/23/too-early/')
       .to_return(status: 200, body: webtoons_post)
 
-    sample_post = sample_file('post_buni.html')
+    sample_post = file_fixture('posts/buni.html').read
     stub_request(:get, %r{^http://www.bunicomic.com})
       .to_return(status: 200, body: sample_post)
   end
