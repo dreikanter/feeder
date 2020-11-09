92d74a3588af7e1972a9148ff9640d13aaf5c63c
Alex Musayev <alex.musayev@gmail.com>
2020-11-09 12:13:06 +0300

    Test buni feed

--- test/support/feed_test_helper.rb
+++ test/support/feed_test_helper.rb
@@ -39,16 +39,18 @@ module FeedTestHelper
     data.map { |item| normalize(item) }
   end
 
-  def notmalize(item)
+  def normalize(item)
     replacements = {
       'published_at' => DateTime.parse(item.fetch('published_at')),
       'feed_id' => feed.id
     }
 
-    NormalizedEntity.new(item.merge(replacements))
+    NormalizedEntity.new(item.merge(replacements).symbolize_keys)
   end
 
   def test_entity_normalization
+    # puts JSON.pretty_generate(expected.map(&:as_json))
+    # puts JSON.pretty_generate(subject.map(&:as_json))
     assert_equal(expected, subject)
   end
 end
