7cd7cdafd89c605bb5bbc994c906092a6414c915
Alex Musayev <alex.musayev@gmail.com>
2021-01-08 18:38:08 +0800

    Debugging

--- config/initializers/update_feeds.rb
+++ config/initializers/update_feeds.rb
@@ -1,12 +1,12 @@
-# TODO: Consider calling UpdateFeeds from elsewhere
-require_relative '../../app/enums/feed_status'
-require_relative '../../app/models/application_record'
-require_relative '../../app/models/feed'
-require_relative '../../app/services/feed_sanitizer'
-require_relative '../../app/services/update_feeds'
+# # TODO: Consider calling UpdateFeeds from elsewhere
+# require_relative '../../app/enums/feed_status'
+# require_relative '../../app/models/application_record'
+# require_relative '../../app/models/feed'
+# require_relative '../../app/services/feed_sanitizer'
+# require_relative '../../app/services/update_feeds'
 
-begin
-  UpdateFeeds.call
-rescue StandardError => e
-  Rails.logger.warn(e)
-end
+# begin
+#   UpdateFeeds.call
+# rescue StandardError => e
+#   Rails.logger.warn(e)
+# end
