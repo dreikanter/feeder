f535e30a496779f3ca9c9bbb0490d23709b3a0a6
Alex Musayev <alex.musayev@gmail.com>
2021-01-10 23:08:27 +0800

    Update loader resolution logic

--- test/services/loader_resolver_test.rb
+++ test/services/loader_resolver_test.rb
@@ -2,37 +2,44 @@ require 'test_helper'
 
 class LoaderResolverTest < Minitest::Test
   UNRESOLVABLE = 'unresolvable'.freeze
+  URL = 'https://example.com/feed'.freeze
 
-  def assert_resolve(expected, attributes)
-    feed = Feed.new(attributes)
-    result = LoaderResolver.call(feed)
-    assert_equal(expected, result)
+  def subject(attributes)
+    LoaderResolver.call(Feed.new(attributes))
   end
 
-  DEFAULT_LOADER = HttpLoader
-
   def test_default_loader_resolution
-    attributes = {}
-    assert_resolve(DEFAULT_LOADER, attributes)
+    attributes = { url: URL }
+    assert_equal(HttpLoader, subject(attributes))
   end
 
   def test_resolution_by_loader_name
-    attributes = { loader: 'twitter' }
-    assert_resolve(TwitterLoader, attributes)
+    attributes = { url: URL, loader: 'twitter' }
+    assert_equal(TwitterLoader, subject(attributes))
   end
 
   def test_resolution_by_feed_name
-    attributes = { name: 'twitter' }
-    assert_resolve(TwitterLoader, attributes)
+    attributes = { url: URL, name: 'twitter' }
+    assert_equal(TwitterLoader, subject(attributes))
   end
 
   def test_loader_name_should_prevail_over_feed_name
-    attributes = { name: UNRESOLVABLE, loader: 'twitter' }
-    assert_resolve(TwitterLoader, attributes)
+    attributes = { url: URL, name: UNRESOLVABLE, loader: 'twitter' }
+    assert_equal(TwitterLoader, subject(attributes))
   end
 
   def test_fallback
-    attributes = { name: UNRESOLVABLE, loader: UNRESOLVABLE }
-    assert_resolve(DEFAULT_LOADER, attributes)
+    attributes = { url: URL, name: UNRESOLVABLE, loader: UNRESOLVABLE }
+    assert_equal(HttpLoader, subject(attributes))
+  end
+
+  def test_null_loader_when_no_url
+    attributes = { url: nil }
+    assert_equal(NullLoader, subject(attributes))
+  end
+
+  def test_null_loader_overrides_feed_loader
+    attributes = { url: nil, loader: 'http' }
+    assert_equal(NullLoader, subject(attributes))
   end
 end
