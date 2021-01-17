0fa77ce07248ab7047d130a231e7487daeb3cb9e
Alex Musayev <alex.musayev@gmail.com>
2021-01-17 15:19:58 +0800

    Test WIP

---
+++ app/normalizers/lobsters_normalizer.rb
@@ -0,0 +1,19 @@
+class LobstersNormalizer < FeedjiraNormalizer
+  protected
+
+  def link
+    content.url
+  end
+
+  def published_at
+    content.published
+  end
+
+  def text
+    [content.title, content.url].join(separator)
+  end
+
+  def comments
+    [content.content]
+  end
+end
