4838582ae1096d9ccb8b1562a03e20fc72d65f0b
Alex Musayev <alex.musayev@gmail.com>
2021-01-23 13:53:58 +0800

    Fix HTTP request stub

--- test/feeds/twitter_test.rb
+++ test/feeds/twitter_test.rb
@@ -7,7 +7,7 @@ class TwitterTest < Minitest::Test
     super
 
     # SEE: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/api-reference/get-statuses-user_timeline
-    stub_request(:get, 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=alg_testament')
+    stub_request(:get, 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=alg_testament&tweet_mode=extended')
       .to_return(
         headers: { 'Content-Type' => 'application/json' },
         body: file_fixture('api/twitter/user_timeline.json').read
