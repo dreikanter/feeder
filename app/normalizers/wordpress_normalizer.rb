deccfcd87eef79c9e2dadd530e69ededbd927deb
Alex Musayev <alex.musayev@gmail.com>
2021-05-27 17:26:59 +0300

    Tatireti feed

---
+++ app/normalizers/wordpress_normalizer.rb
@@ -0,0 +1,34 @@
+class WordpressNormalizer < BaseNormalizer
+  protected
+
+  def link
+    content.url
+  end
+
+  def published_at
+    content.published
+  end
+
+  def text
+    [content.title, link].join(separator)
+  end
+
+  def attachments
+    [image_url]
+  end
+
+  def comments
+    [description]
+  end
+
+  private
+
+  def image_url
+    Html.first_image_url(content.content)
+  end
+
+  def description
+    excerpt = Html.squeeze(content.content)
+    Html.comment_excerpt(excerpt)
+  end
+end
