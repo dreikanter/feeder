e8ae5a8364672ccc5cb826ff12cc741a5eacb6be
Alex Musayev <alex.musayev@gmail.com>
2021-05-27 17:27:48 +0300

    Add tatireti feed (#303)
    * Annotations update
    
    * Tatireti feed

--- test/factories/error_factory.rb
+++ test/factories/error_factory.rb
@@ -1,3 +1,31 @@
+# == Schema Information
+#
+# Table name: errors
+#
+#  id          :integer          not null, primary key
+#  status      :integer          default("pending"), not null
+#  exception   :string           default(""), not null
+#  file_name   :string
+#  line_number :integer
+#  label       :string           default(""), not null
+#  message     :string           default(""), not null
+#  backtrace   :string           default([]), not null, is an Array
+#  context     :json             not null
+#  occured_at  :datetime         not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  target_type :string
+#  target_id   :bigint
+#
+# Indexes
+#
+#  index_errors_on_exception                  (exception)
+#  index_errors_on_file_name                  (file_name)
+#  index_errors_on_occured_at                 (occured_at)
+#  index_errors_on_status                     (status)
+#  index_errors_on_target_type_and_target_id  (target_type,target_id)
+#
+
 FactoryBot.define do
   factory :error, class: Error do
     context { {} }
