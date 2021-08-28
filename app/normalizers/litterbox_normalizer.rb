a7c075444535f9960a60a85ebdabf7902d84a68c
Alex Musayev <alex.musayev@gmail.com>
2021-08-28 10:13:25 +0300

    Litterbox feed (#313)
    * Update gems
    
    * Litterbox feed
    
    * Use correct example for bonus panel page
    
    * Simplify CSS celector
    
    * Fix test expectation
    
    * Test slides version
    
    * Update normalizer
    
    * Temporarily increase import limit
    
    * Skip bonus entities
    
    * Fix bonus panel extraction
    
    * Linters

---
+++ app/normalizers/litterbox_normalizer.rb
@@ -0,0 +1,76 @@
+class LitterboxNormalizer < WordpressNormalizer
+  protected
+
+  def link
+    content.url
+  end
+
+  def published_at
+    content.published
+  end
+
+  def text
+    [content.title, link].join(separator)
+  end
+
+  def attachments
+    return extract_slides if slides?
+    [extract_single_image]
+  end
+
+  def comments
+    [bonus_panel_comment].compact
+  end
+
+  private
+
+  def extract_slides
+    page_html.css('.swiper-wrapper img').map { |element| element['src'] }
+  end
+
+  def page_html
+    @page_html ||= Nokogiri::HTML(RestClient.get(link).body)
+  end
+
+  def slides?
+    page_html.css('.swiper-wrapper').present?
+  end
+
+  def extract_single_image
+    Html.first_image_url(content.content)
+  end
+
+  def description
+    excerpt = Html.squeeze(content.content)
+    Html.comment_excerpt(excerpt)
+  end
+
+  def bonus_panel_comment
+    url = bonus_panel_image_url
+    "Bonus panel: #{url}" if url
+  end
+
+  def bonus_panel_image_url
+    html = RestClient.get(bonus_panel_page_url).body
+    Nokogiri::HTML(html).css('meta[property=og\:image]').first['content']
+  rescue StandardError
+    nil
+  end
+
+  # :reek:FeatureEnvy
+  def bonus_panel_page_url
+    Addressable::URI.parse(link).tap do |uri|
+      uri.path = uri.path.gsub(%r{/$}, '').gsub(/$/, '-bonus/')
+    end.to_s
+  end
+
+  def validation_errors
+    super.tap do |errors|
+      errors << 'bonus' if bonus?
+    end
+  end
+
+  def bonus?
+    link =~ %r{-bonus/?$}
+  end
+end
