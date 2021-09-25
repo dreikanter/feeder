4e310b2d841aacece4b084733351c85713ce7507
Alex Musayev <alex.musayev@gmail.com>
2021-09-25 20:15:16 +0300

    Extract XKCD image URL from OpenGraph meta tag (#331)
    * Extract XKCD image URL from OpenGraph meta tag
    
    * Rubocop
    
    * Update feed test

--- app/normalizers/xkcd_normalizer.rb
+++ app/normalizers/xkcd_normalizer.rb
@@ -6,13 +6,37 @@ class XkcdNormalizer < RssNormalizer
   end
 
   def attachments
-    [image['src']]
+    [image_url]
   end
 
   def comments
     alt = image['alt'].to_s
     return [] if alt.empty?
     [Html.comment_excerpt(Html.squeeze(alt))]
+  end
+
+  private
+
+  def image_url
+    fetch_og_image_url
+  rescue StandardError
+    feed_entry_image_url
+  end
+
+  def fetch_og_image_url
+    html.css('meta[property="og:image"]').first.attributes['content'].value
+  end
+
+  def html
+    Nokogiri::HTML(page_content)
+  end
+
+  def page_content
+    RestClient.get(link).body
+  end
+
+  def feed_entry_image_url
+    image.first['src']
   end
 
   def image
