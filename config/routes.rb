6e3e60054e2c0023054efe0f41e2575c47caf1c7
Alex Musayev <alex.musayev@gmail.com>
2021-10-02 14:12:17 +0300

    Remove less-used routing configuration in favor to simplified dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    resources :feeds, only: %i[index show], param: :name do
-      resource :activity, only: :show, controller: :activity
-      resources :posts, only: :index
-      resources :updates, only: :index
-    end
-
-    resource :activity, only: :show, controller: :activity
-    resources :batches, only: :index
-    resources :posts, only: :index
-    resources :updates, only: :index
-  end
-
-  resources(
-    :feeds,
-    only: %i[index show],
-    controller: :layout,
-    action: :show,
-    param: :name
-  )
-
-  resources :posts, only: :index, controller: :layout, action: :show
-  resources :updates, only: :index, controller: :layout, action: :show
-
-  root 'layout#show'
-
-  get :about, controller: :layout, action: :show
+  root 'dashboard#show'
 end
