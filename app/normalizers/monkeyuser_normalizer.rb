2215820b3a6ee894e71026adaf5d33f0535b26c7
Alex Musayev <alex.musayev@gmail.com>
2021-12-01 15:55:01 +0300

    Add image alt text to the monkeyuser comments

--- app/normalizers/monkeyuser_normalizer.rb
+++ app/normalizers/monkeyuser_normalizer.rb
@@ -9,6 +9,18 @@ class MonkeyuserNormalizer < FeedjiraNormalizer
     [image_url]
   end
 
+  def comments
+    [image_title]
+  end
+
+  def image_title
+    first_image['title']
+  end
+
+  def first_image
+    @first_image ||= Nokogiri::HTML(content.summary).css('img:first').first
+  end
+
   def image_url
     Html.first_image_url(content.summary)
   end
