f986c297058d357679a3b4df8a4fc25abfb38774
Alex Musayev <alex.musayev@gmail.com>
2023-06-05 08:51:44 +0200

    Introduce Nitter loader

---
+++ app/loaders/nitter_loader.rb
@@ -0,0 +1,21 @@
+class NitterLoader < BaseLoader
+  DEFAULT_NITTER_URL = "https://nitter.net".freeze
+
+  option(:nitter_url, optional: true, default: -> { DEFAULT_NITTER_URL })
+
+  protected
+
+  def perform
+    RestClient.get(nitter_rss_url.to_s).body
+  end
+
+  private
+
+  def nitter_rss_url
+    URI.parse(nitter_url).merge("/#{twitter_user}/rss")
+  end
+
+  def twitter_user
+    feed.options.fetch("twitter_user")
+  end
+end
