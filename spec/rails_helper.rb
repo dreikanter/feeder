adba95636029aac4b7289dad784b97c1ae0a385e
Alex Musayev <alex.musayev@gmail.com>
2023-06-09 13:43:06 +0200

    Spacing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,7 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
