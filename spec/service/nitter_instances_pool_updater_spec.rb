dcc6093defb4e09e4f3282d870d5e435f577f47f
Alex Musayev <alex.musayev@gmail.com>
2023-06-10 16:58:33 +0200

    Fix Nitter instances fetched spec

---
+++ spec/service/nitter_instances_pool_updater_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe NitterInstancesPoolUpdater do
+  subject(:service) { described_class }
+
+  let(:expected) { JSON.parse(file_fixture('nitter_instances_wiki_page.json').read) }
+
+  before do
+    stub_request(:get, NitterInstancesFetcher::PUBLIC_INSTANCES_WIKI_PAGE_URL)
+      .to_return(body: file_fixture('nitter_instances_wiki_page.html').read)
+  end
+
+  it 'import instances' do
+    service.call
+    imported_urls = NitterInstance.where(status: :enabled).pluck(:url)
+    expect(imported_urls).to contain_exactly(*expected)
+  end
+
+  it 'be idempotent' do
+    service.call
+    expect { service.call }.not_to change { NitterInstance.pluck(:id, :url, :status) }
+  end
+end
