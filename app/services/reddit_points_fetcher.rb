16ddee73c5a346d8be8ededd0ea0caad5acc14a0
Alex Musayev <alex.musayev@gmail.com>
2023-06-10 20:25:52 +0200

    Add more Libreddit hosts

--- app/services/reddit_points_fetcher.rb
+++ app/services/reddit_points_fetcher.rb
@@ -4,6 +4,7 @@ class RedditPointsFetcher
   param :url
 
   def call
+    Honeybadger.context(libreddit_host: libreddit_host, short_url: short_url, libreddit_url: libreddit_url)
     Integer(dom.css('.post_score').attribute('title').value)
   end
 
@@ -13,27 +14,75 @@ class RedditPointsFetcher
     Nokogiri::HTML(page_content)
   end
 
+  # TODO: Replace with dynamically updated list
+  # SEE: https://github.com/libreddit/libreddit-instances/blob/master/instances.md
   LIBREDDIT_HOSTS = %w[
-    safereddit.com
-    libreddit.kavin.rocks
-    lr.riverside.rocks
-    reddit.baby
-    libreddit.de
-    libreddit.hu
-    libreddit.nl
+    https://safereddit.com
+    https://libreddit.kavin.rocks
+    https://reddit.invak.id
+    https://reddit.simo.sh
+    https://lr.riverside.rocks
+    https://libreddit.strongthany.cc
+    https://libreddit.privacy.com.de
+    https://reddit.baby
+    https://libreddit.domain.glass
+    https://r.nf
+    https://lr.mint.lgbt
+    https://libreddit.pussthecat.org
+    https://libreddit.northboot.xyz
+    https://libreddit.hu
+    https://libreddit.totaldarkness.net
+    https://lr.vern.cc
+    https://libreddit.nl
+    https://reddi.tk
+    https://r.walkx.fyi
+    https://libreddit.kylrth.com
+    https://libreddit.tiekoetter.com
+    https://reddit.rtrace.io
+    https://libreddit.privacydev.net
+    https://r.ahwx.org
+    https://reddit.dr460nf1r3.org
+    https://l.opnxng.com
+    https://libreddit.cachyos.org
+    https://rd.funami.tech
+    https://libreddit.projectsegfau.lt
+    https://lr.slipfox.xyz
+    https://libreddit.oxymagnesium.com
+    https://reddit.utsav2.dev
+    https://libreddit.freedit.eu
+    https://libreddit.mha.fi
+    https://libreddit.garudalinux.org
+    https://lr.4201337.xyz
+    https://lr.odyssey346.dev
+    https://lr.artemislena.eu
+    https://discuss.whatever.social
+    https://libreddit.pufe.org
+    https://lr.aeong.one
+    https://reddit.smnz.de
+    https://reddit.leptons.xyz
+    https://libreddit.lunar.icu
+    https://reddit.moe.ngo
+    https://r.darklab.sh
+    https://snoo.habedieeh.re
+    https://libreddit.kutay.dev
+    https://libreddit.tux.pizza
   ].freeze
 
   private_constant :LIBREDDIT_HOSTS
 
   def page_content
-    RestClient.get(libreddit_url).body
+    HTTP.get(libreddit_url).body.to_s
   end
 
   def libreddit_url
-    URI.parse(short_url).tap { |parsed| parsed.host = LIBREDDIT_HOSTS.sample }.to_s
+    URI.parse(short_url).tap { |parsed| parsed.host = libreddit_host }.to_s
   end
 
   def short_url
     RedditSlugsChopper.call(url)
+  end
+
+  def libreddit_host
+    @libreddit_host ||= URI.parse(LIBREDDIT_HOSTS.sample).host
   end
 end
--- spec/loaders/nitter_loader_spec.rb
+++ spec/loaders/nitter_loader_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe NitterLoader do
 
   let(:feed) { build(:feed, options: { 'twitter_user' => 'username' }) }
   let(:body) { 'CONTENT BODY' }
-  let(:nitter_url) { %r{^https://.*/username/rss$} }
+  let(:url) { %r{^https://.*/username/rss$} }
 
   it 'fetches nitter url' do
-    stub_request(:get, nitter_url).to_return(body: body)
+    stub_request(:get, url).to_return(body: body)
     result = loader.call(feed)
     expect(body).to eq(result)
   end
 
   it 'passes HTTP client error' do
-    stub_request(:get, nitter_url).to_raise(StandardError)
+    stub_request(:get, url).to_raise(StandardError)
     assert_raises(StandardError) { loader.call(feed) }
   end
 
