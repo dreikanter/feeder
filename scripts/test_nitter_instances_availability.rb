46d988534fdb4f7636c65b41c0f3a6e816e7c64d
Alex Musayev <alex.musayev@gmail.com>
2023-06-10 16:28:45 +0200

    Remove non-available Nitter instances

--- scripts/test_nitter_instances_availability.rb
+++ scripts/test_nitter_instances_availability.rb
@@ -1,25 +1,29 @@
-class NitterLoader < BaseLoader
-  # TODO: Replace with dynamically updated list
-  # SEE: https://github.com/zedeus/nitter/wiki/Instances
-  NITTER_INSTANCES = %w[
+puts "Available Nitter instances:\n\n"
+
+%w[
     https://nitter.net
     https://nitter.lacontrevoie.fr
     https://nitter.fdn.fr
     https://nitter.1d4.us
     https://nitter.kavin.rocks
+    https://nitter.unixfox.eu
     https://nitter.moomoo.me
     https://nitter.it
+    https://twitter.censors.us
     https://twitter.owacon.moe
     https://nitter.fly.dev
     https://notabird.site
     https://nitter.sethforprivacy.com
     https://nitter.cutelab.space
+    https://nitter.nl
     https://nitter.mint.lgbt
     https://nitter.esmailelbob.xyz
     https://tw.artemislena.eu
+    https://nitter.tiekoetter.com
     https://nitter.privacy.com.de
     https://nitter.bird.froth.zone
     https://twitter.dr460nf1r3.org
+    https://nitter.garudalinux.org
     https://nitter.cz
     https://nitter.privacydev.net
     https://tweet.lambda.dance
@@ -58,15 +62,21 @@ class NitterLoader < BaseLoader
     https://nitter.lunar.icu
     https://twitter.moe.ngo
     https://nitter.freedit.eu
+    https://ntr.frail.duckdns.org
     https://n.opnxng.com
+    https://nitter.plus.st
     https://nitter.in.projectsegfau.lt
     https://nitter.tux.pizza
     https://t.floss.media
+    https://twit.hell.rodeo
+    https://nitter.nachtalb.io
     https://n.quadtr.ee
     https://nitter.altgr.xyz
     https://jote.lile.cl
     https://nitter.one
+    https://nitter.instance.no
     https://nitter.io.lol
+    https://nitter.hu
     https://nitter.no-logs.com
     https://nitter.fascinated.cc
     https://nitter.kozmik.studio
@@ -76,32 +86,12 @@ class NitterLoader < BaseLoader
     https://nitter.nohost.network
     https://nitter.services.woodland.cafe
     https://twt.funami.tech
-  ].freeze
+].each do |url|
+  rss_url = URI.parse(url).merge("/_yesbut_/rss").to_s
 
-  option(:nitter_url, optional: true, default: -> { NITTER_INSTANCES.sample })
-
-  protected
-
-  def perform
-    RestClient.get(nitter_rss_url.to_s).body
-  rescue StandardError => e
-    # TODO: Do not treat 404 as instance availability
-    ErrorDumper.call(exception: e, message: 'Nitter error', target: feed)
-    register_nitter_instance_error
-    raise
-  end
-
-  private
-
-  def register_nitter_instance_error
-    NitterInstance.find_or_create_by(url: nitter_url).error!
-  end
-
-  def nitter_rss_url
-    URI.parse(nitter_url).merge("/#{twitter_user}/rss")
-  end
-
-  def twitter_user
-    feed.options.fetch('twitter_user')
+  begin
+    RestClient.get(rss_url).body
+    puts url
+  rescue => e
   end
 end
