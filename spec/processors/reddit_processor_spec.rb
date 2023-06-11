268789de84cc1662af1d2915c0717cc0456cd325
Alex Musayev <alex.musayev@gmail.com>
2023-06-11 17:43:37 +0200

    TEst posts score service errors handling

--- spec/processors/reddit_processor_spec.rb
+++ spec/processors/reddit_processor_spec.rb
@@ -16,22 +16,26 @@ RSpec.describe RedditProcessor do
   end
 
   let(:expected_uids) { expected_data_points.map { |details| details["link"] } }
-
   let(:expected_data_points) { JSON.parse(file_fixture("feeds/reddit/expected_data_points.json").read) }
+  let(:thread_url) { %r{^https://.*/r/worldnews/comments/} }
+  let(:thread_contents) { file_fixture('feeds/reddit/libreddit_comments_page.html').read }
 
-  before do
-    DataPoint.delete_all
-
-    stub_request(:get, %r{^https://.*/r/worldnews/comments/})
-      .to_return(body: file_fixture('feeds/reddit/libreddit_comments_page.html').read)
-  end
+  before { DataPoint.delete_all }
 
   it 'fetches reddit points' do
+    stub_request(:get, thread_url).to_return(body: thread_contents)
     expect(call_processor.map(&:uid)).to eq(expected_uids)
   end
 
   it 'creates data points' do
+    stub_request(:get, thread_url).to_return(body: thread_contents)
     expect { call_processor }.to(change { DataPoint.all.pluck(:details) }.from([]).to(expected_data_points))
+  end
+
+  it 'ignores posts on score service error' do
+    stub_request(:get, thread_url).to_return(status: 404)
+    expect(call_processor).to be_empty
+    expect( DataPoint.all.pluck(:details)).to be_empty
   end
 
   def call_processor
