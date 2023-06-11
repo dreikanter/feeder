64a7758f932c7d7fed88111a8143f45f63ce86ee
Alex Musayev <alex.musayev@gmail.com>
2023-06-11 14:38:46 +0200

    Refactor `HttpLoader` with HTTP.rb

--- app/loaders/http_loader.rb
+++ app/loaders/http_loader.rb
@@ -2,6 +2,23 @@ class HttpLoader < BaseLoader
   protected
 
   def perform
-    RestClient.get(feed.url).body
+    ensure_successful_respone
+    response.to_s
+  end
+
+  private
+
+  def ensure_successful_respone
+    return if response.status == 200
+    define_error_context
+    raise Error
+  end
+
+  def define_error_context
+    Honeybadger.context(http_response: response.as_json)
+  end
+
+  def response
+    @response ||= HTTP.get(feed.url)
   end
 end
---
+++ spec/loaders/http_loader_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe HttpLoader do
+  subject(:loader) { described_class }
+
+  let(:feed) { build(:feed) }
+  let(:content) { "BANANA" }
+  let(:arbitrary_error) { "arbitrary error object" }
+
+  it 'requires feed url to present' do
+    feed.url = nil
+    expect { subject.call(feed) }.to raise_error(HTTP::Error)
+  end
+
+  it 'requires explicit scheme' do
+    feed.url = URI.parse(feed.url).tap { |parsed| parsed.scheme = nil }.to_s
+    expect { subject.call(feed) }.to raise_error(HTTP::Error)
+  end
+
+  it 'returns successful response body' do
+    stub_request(:get, feed.url).to_return(body: content)
+    expect(subject.call(feed)).to eq(content)
+  end
+
+  it 'raises on error response' do
+    stub_request(:get, feed.url).to_return(status: 404)
+    expect { subject.call(feed) }.to raise_error(BaseLoader::Error)
+  end
+
+  it 'passes request execution errors' do
+    stub_request(:get, feed.url).to_raise(arbitrary_error)
+    expect { subject.call(feed) }.to raise_error(arbitrary_error)
+  end
+end
