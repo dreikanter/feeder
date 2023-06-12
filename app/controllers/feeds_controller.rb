9a4dea3df4185f4504791cfa4993afd277589c98
Alex Musayev <alex.musayev@gmail.com>
2023-06-12 22:12:45 +0200

    Count active feeds in controller

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,19 +1,24 @@
 class FeedsController < ApplicationController
   def index
-    render(locals: show_locals)
+    render(locals: locals)
   end
 
   private
 
-  def show_locals
+  def locals
     {
       feeds: Feed.ordered_by(order_by, order),
+      active_feeds_count: active_feeds_count,
       last_post_created_at: last_post_created_at,
       last_update: last_update,
       order: order,
       order_by: order_by,
       alt_order_direction: alt_order_direction
     }
+  end
+
+  def active_feeds_count
+    Feed.active.count
   end
 
   def last_post_created_at
@@ -25,7 +30,7 @@ class FeedsController < ApplicationController
   end
 
   def alt_order_direction
-    order == 'asc' ? 'desc' : 'asc'
+    order == 'asc' ? 'desc' : order
   end
 
   def order_by
