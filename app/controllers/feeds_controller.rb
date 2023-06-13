07f6fff75963f7d4c0d043e3f3889aa3fcdd8001
Alex Musayev <alex.musayev@gmail.com>
2023-06-13 11:15:12 +0200

    Refactor dashboard queries

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -8,24 +8,12 @@ class FeedsController < ApplicationController
   def locals
     {
       feeds: Feed.ordered_by(order_by, order),
-      active_feeds_count: active_feeds_count,
-      last_post_created_at: last_post_created_at,
-      last_update: last_update,
+      active_feeds_count: Feed.active.count,
+      last_post_created_at: Post.maximum(:created_at),
+      last_update: Feed.maximum(:refreshed_at),
       order: order,
       order_by: order_by
     }
-  end
-
-  def active_feeds_count
-    Feed.active.count
-  end
-
-  def last_post_created_at
-    Post.order(created_at: :desc).first.try(:created_at)
-  end
-
-  def last_update
-    DataPoint.ordered_by_created_at.for("pull").first.try(:created_at)
   end
 
   def order_by
