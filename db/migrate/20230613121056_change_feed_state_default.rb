669dc2d50a34f6ae9540e93be6e2467e451697e2
Alex Musayev <alex.musayev@gmail.com>
2023-06-14 12:05:24 +0200

    Refactor migration

--- db/migrate/20230613121056_change_feed_state_default.rb
+++ db/migrate/20230613121056_change_feed_state_default.rb
@@ -1,14 +1,6 @@
 class ChangeFeedStateDefault < ActiveRecord::Migration[6.1]
   def change
     add_column :feeds, :state_updated_at, :datetime
-
-    reversible do |dir|
-      dir.up do
-        change_column :feeds, :state, :string, null: false, default: "pristine"
-      end
-      dir.down do
-        change_column :feeds, :state, :string, null: false, default: "enabled"
-      end
-    end
+    change_column_default :feeds, :state, from: "enabled", to: "pristine"
   end
 end
