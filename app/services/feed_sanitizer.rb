26fc339e71bdf6646ca5cd24a4534539c076fb12
Alex Musayev <alex.musayev@gmail.com>
2023-06-15 19:00:06 +0200

    Improve `FeedSanitizer` result structure

--- app/services/feed_sanitizer.rb
+++ app/services/feed_sanitizer.rb
@@ -13,17 +13,26 @@ class FeedSanitizer
   option(:disabling_reason, type: Dry::Types["strict.string"], optional: true)
   option(:options, type: Dry::Types["strict.hash"], optional: true, default: -> { {} })
   option(:refresh_interval, type: Dry::Types["strict.integer"], optional: true)
-  option(:enabled, type: Dry::Types["params.bool"], optional: true)
+  option(:enabled, type: Dry::Types["params.bool"], optional: true, default: -> { true })
 
-  # @return [Hash] sanitized feed configuration attributes
+  # @return [Hash] sanitized feed configuration data
   def call
-    option_names
-      .map { |target| [target, send(target)] }
-      .filter { |_, value| value.present? }
-      .to_h
+    {
+      name: name,
+      enabled: enabled,
+      attributes: attributes
+    }
   end
 
   private
+
+  def attributes
+    attribute_option_names.filter_map { |attribute| send(attribute).then { |value| [attribute, value] } }.to_h.compact
+  end
+
+  def attribute_option_names
+    option_names.excluding(:name, :enabled)
+  end
 
   def option_names
     self.class.dry_initializer.options.map(&:target)
