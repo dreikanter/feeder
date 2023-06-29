fb9f48e740a7c6b2b81a4f9f8606f1bcb54e6234
Alex Musayev <alex.musayev@gmail.com>
2023-06-29 19:27:33 +0200

    Rubocop

--- spec/services/purge_errors_spec.rb
+++ spec/services/purge_errors_spec.rb
@@ -3,19 +3,19 @@ require "rails_helper"
 RSpec.describe PurgeErrors do
   subject(:service) { described_class }
 
-  let(:sample_threshold) { 1.months.ago }
+  let(:sample_threshold) { 1.month.ago }
 
   before { freeze_time }
 
   it "deletes old errors" do
     error = create(:error, created_at: sample_threshold - 1.second)
-    subject.call(before: sample_threshold)
+    service.call(before: sample_threshold)
     expect(Error.where(id: error.id)).not_to exist
   end
 
   it "keep recent errors" do
     error = create(:error, created_at: sample_threshold + 1.second)
-    subject.call(before: sample_threshold)
+    service.call(before: sample_threshold)
     expect(Error.where(id: error.id)).to exist
   end
 end
