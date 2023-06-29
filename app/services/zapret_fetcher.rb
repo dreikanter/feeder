78c04a463ff3e7de1c1483bf3cc1cc1df3632ee6
Alex Musayev <alex.musayev@gmail.com>
2023-06-29 17:45:53 +0200

    Drop unused class

--- app/services/zapret_fetcher.rb
+++
@@ -1,8 +0,0 @@
-class ZapretFetcher
-  URL = "https://zapret.info/api/?url=%s".freeze
-
-  def self.call(value)
-    response = RestClient.get(format(URL, CGI.escape(value)))
-    !!JSON.parse(response.body)["isBlocked"]
-  end
-end
