61dfcdfb75361ca12c1b29732a17ffc76d68a018
Alex Musayev <alex.musayev@gmail.com>
2023-06-29 18:58:26 +0200

    Remove subscription data points

--- app/services/update_subscriptions_count.rb
+++ app/services/update_subscriptions_count.rb
@@ -4,45 +4,16 @@ class UpdateSubscriptionsCount
   param :feed_name
   option :freefeed_client, optional: true, default: -> { FreefeedClientBuilder.call }
 
-  SERIES_NAME = :subs
-
   def call
-    update_or_create_data_point
-    update_feed
+    feed.update(subscriptions_count: fetch_subscriptions_count)
   end
 
   private
 
-  def update_or_create_data_point
-    return unless prev_count != current_count
-    CreateDataPoint.call(:subs, details: {feed_name: feed_name, count: current_count})
-  end
-
-  def prev_count
-    prev_data_point.try(:details).try(:[], "count")
-  end
-
-  def prev_data_point
-    DataPoint
-      .for(SERIES_NAME)
-      .where("details->>'feed_name' = ?", feed_name)
-      .order(created_at: :desc)
-      .first
-  end
-
-  def current_count
-    @current_count ||= fetch_current_count
-  end
-
-  def fetch_current_count
-    Rails.logger.info("fetching group details: #{feed_name}")
+  def fetch_subscriptions_count
     timeline = freefeed_client.timeline(feed_name)
     subscribers = timeline.parse.dig("timelines", "subscribers")
     subscribers.count
-  end
-
-  def update_feed
-    feed.update(subscriptions_count: current_count)
   end
 
   def feed
