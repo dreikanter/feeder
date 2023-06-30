c5e64ffc815a1a3b9bd138b0121393e537d7a5d4
Alex Musayev <alex.musayev@gmail.com>
2023-06-30 13:03:57 +0200

    Fix the test

--- spec/normalizers/nextbigfuture_normalizer_spec.rb
+++ spec/normalizers/nextbigfuture_normalizer_spec.rb
@@ -4,22 +4,14 @@ RSpec.describe NextbigfutureNormalizer do
   subject(:normalizer) { described_class }
 
   let(:feed) { create(:feed, :nextbigfuture) }
-  let(:expected_entity) do
-    JSON.parse(file_fixture("feeds/nextbigfuture/entity.json").read).tap do |data|
-      data["feed_id"] = feed.id
-      data["published_at"] = Time.zone.parse(data["published_at"])
-    end
-  end
   let(:content) { file_fixture("feeds/nextbigfuture/feed.xml").read }
   let(:entities) { feed.processor_class.call(content: content, feed: feed) }
   let(:normalized_entries) { entities.map { |entity| normalizer.call(entity) } }
 
-  let(:expected) do
-    JSON.parse(file_fixture("feeds/kotaku/normalized.json").read).map do |data|
-      data.merge(
-        "feed_id" => feed.id,
-        "published_at" => DateTime.parse(data["published_at"])
-      )
+  let(:expected_entity) do
+    JSON.parse(file_fixture("feeds/nextbigfuture/entity.json").read).tap do |data|
+      data["feed_id"] = feed.id
+      data["published_at"] = Time.zone.parse(data["published_at"])
     end
   end
 
