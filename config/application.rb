fa38898f44c830b8789764573bd1c38023d7b3fb
Alex Musayev <alex.musayev@gmail.com>
2023-07-11 11:45:31 +0200

    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,18 @@
 require_relative "boot"
+
 require "rails"
-
-# SEE: https://github.com/rails/rails/blob/6-0-stable/railties/lib/rails/all.rb
-# require 'rails/all'
-
-require "active_record/railtie"
-# require 'active_storage/engine'
-require "action_controller/railtie"
-require "action_view/railtie"
-# require 'action_mailer/railtie'
+# Pick the frameworks you want:
+require "active_model/railtie"
 require "active_job/railtie"
-# require 'action_cable/engine'
-# require 'action_mailbox/engine'
-# require 'action_text/engine'
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
 require "rails/test_unit/railtie"
-# require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,27 +20,15 @@ Bundler.require(*Rails.groups)
 
 module Feeder
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.autoload_paths += %w[app lib].map { |path| config.root.join(path) }
-    config.add_autoload_paths_to_load_path = false
-    config.active_support.cache_format_version = 6.1
 
-    config.hosts << "feeder.local"
-    config.hosts << "frf.im"
-    config.hosts << "localhost"
-    config.hosts << "app"
-
-    config.generators do |generate|
-      generate.test_framework :rspec, fixture: false
-      generate.controller_specs true
-      generate.decorator false
-      generate.helper false
-      generate.helper_specs false
-      generate.javascripts false
-      generate.routing_specs false
-      generate.skip_routes true
-      generate.stylesheets false
-      generate.view_specs false
-    end
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
