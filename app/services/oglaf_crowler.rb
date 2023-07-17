7d08b2563581fc8ad3d5fbee200a795fd6a96547
Alex Musayev <alex.musayev@gmail.com>
2023-07-17 08:49:22 +0200

    Refactor a service class to improve test coverage

--- app/services/oglaf_crowler.rb
+++ app/services/oglaf_crowler.rb
@@ -5,32 +5,36 @@ class OglafCrowler
   # Hard limit by Freefeed API
   MAX_ATTACHMENTS_COUNT = 20
 
-  def self.call(url)
-    next_url = url
-    pages = []
+  class << self
+    def call(url)
+      next_url = url
+      pages = []
 
-    while next_url && pages.count < MAX_ATTACHMENTS_COUNT
-      response = RestClient.get(next_url)
-      html = Nokogiri::HTML(response.body)
-      pages << html
-      next_url = next_page_url(html)
+      while next_url && pages.count < MAX_ATTACHMENTS_COUNT
+        response = RestClient.get(next_url)
+        html = Nokogiri::HTML(response.body)
+        pages << html
+        next_url = next_page_url(html)
+      end
+
+      pages
     end
 
-    pages
-  end
+    private
 
-  def self.next_page_url(html)
-    result = html.css("link[rel=next]")
-      .try(:first)
-      .try(:attributes)
-      .try(:[], "href")
-      .try(:value)
+    def next_page_url(html)
+      result = html.css("link[rel=next]")
+        .try(:first)
+        .try(:attributes)
+        .try(:[], "href")
+        .try(:value)
 
-    return nil if result.blank? || !result.match(%r{/\d+/$})
+      return nil if result.blank? || !result.match(%r{/\d+/$})
 
-    uri = Addressable::URI.parse(result)
-    uri.host ||= HOST
-    uri.scheme ||= SCHEME
-    uri.to_s
+      uri = Addressable::URI.parse(result)
+      uri.host ||= HOST
+      uri.scheme ||= SCHEME
+      uri.to_s
+    end
   end
 end
