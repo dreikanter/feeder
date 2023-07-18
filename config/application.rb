91cdf449d9e0b3b3104096428018e2f938c346b1
Alex Musayev <alex.musayev@gmail.com>
2023-07-18 20:52:06 +0200

    Honeybadger HTTP requests tracking

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# TODO: Use autoload
+require_relative "../app/support/http/features/request_tracking"
+
 module Feeder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
