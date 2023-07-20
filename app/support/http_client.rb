8ab6de108c195ce2225ecc944f7dd9b28a3a1db5
Alex Musayev <alex.musayev@gmail.com>
2023-07-20 20:36:33 +0200

    Ensure HTTP feature is loaded

--- app/support/http_client.rb
+++ app/support/http_client.rb
@@ -1,6 +1,10 @@
 module HttpClient
   # :reek:UtilityFunction
   def http
+    unless HTTP::Options.available_features.key?(:request_tracking)
+      HTTP::Options.register_feature(:request_tracking, RequestTracking)
+    end
+
     HTTP.use(:request_tracking)
   end
 end
