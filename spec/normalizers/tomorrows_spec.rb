bd2bd911f21856a4dcd8afc5d3d7e0a9a38f8726
Alex Musayev <alex.musayev@gmail.com>
2023-07-21 10:52:37 +0200

    Test `TomorrowsNormalizer` and `TheAtlanticPhotosNormalizer`

--- spec/normalizers/tomorrows_spec.rb
+++ spec/normalizers/tomorrows_spec.rb
@@ -1,34 +1,24 @@
 require "rails_helper"
 require "support/shared_examples_a_normalizer"
 
-# class TomorrowsTest < Minitest::Test
-#   include FeedTestHelper
+RSpec.describe TomorrowsNormalizer do
+  subject(:subject_name) { described_class }
 
-#   def feed_config
-#     {
-#       name: "365tomorrows",
-#       loader: "http",
-#       processor: "rss",
-#       normalizer: "tomorrows",
-#       url: "http://365tomorrows.com/feed/"
-#     }
-#   end
+  it_behaves_like "a normalizer" do
+    let(:feed) do
+      create(
+        :feed,
+        name: "365tomorrows",
+        loader: "http",
+        processor: "rss",
+        normalizer: "tomorrows",
+        url: "http://365tomorrows.com/feed/"
+      )
+    end
 
-#   def setup
-#     super
-
-#     stub_request(:get, %r{https://365tomorrows.com/\d+/.*})
-#       .to_return(
-#         body: file_fixture("feeds/tomorrows/post.html"),
-#         headers: {"Content-Type" => "text/html"}
-#       )
-#   end
-
-#   def source_fixture_path
-#     "feeds/tomorrows/feed.xml"
-#   end
-
-#   def expected_fixture_path
-#     "feeds/tomorrows/entity.json"
-#   end
-# end
+    before do
+      stub_request(:get, %r{https://365tomorrows.com/\d+/.*})
+        .to_return(body: file_fixture("feeds/365tomorrows/post.html"))
+    end
+  end
+end
