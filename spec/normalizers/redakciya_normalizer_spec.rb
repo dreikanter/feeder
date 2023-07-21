a62a47ef4b6716ac7419f31e08d01c100272141b
Alex Musayev <alex.musayev@gmail.com>
2023-07-21 10:41:37 +0200

    Test `RedakciyaNormalizer`

--- spec/normalizers/redakciya_normalizer_spec.rb
+++ spec/normalizers/redakciya_normalizer_spec.rb
@@ -4,36 +4,16 @@ require "support/shared_examples_a_normalizer"
 RSpec.describe RedakciyaNormalizer do
   subject(:subject_name) { described_class }
 
-  # TBD
+  it_behaves_like "a normalizer" do
+    let(:feed) do
+      create(
+        :feed,
+        name: "redakciya",
+        url: "https://www.youtube.com/feeds/videos.xml?channel_id=UC1eFXmJNkjITxPFWTy6RsWg",
+        loader: "http",
+        processor: "youtube",
+        normalizer: "redakciya"
+      )
+    end
+  end
 end
-
-# class RedakciyaNormalizerTest < Minitest::Test
-#   include NormalizerTestHelper
-
-#   def subject
-#     RedakciyaNormalizer
-#   end
-
-#   def processor
-#     YoutubeProcessor
-#   end
-
-#   def sample_data_file
-#     "feed_redakciya.xml"
-#   end
-
-#   def feed
-#     build(:feed, options: {description: false})
-#   end
-
-#   def expected
-#     entity = JSON.parse(file_fixture("feeds/redakciya/entity.json").read)
-#     value = entity["published_at"]
-#     entity["published_at"] = DateTime.parse(value) if value
-#     NormalizedEntity.new(**entity.symbolize_keys.merge(feed_id: feed.id))
-#   end
-
-#   def test_normalized_sample
-#     assert_equal(expected, normalized.first)
-#   end
-# end
