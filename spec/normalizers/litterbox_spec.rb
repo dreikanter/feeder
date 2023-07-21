70b2bfcb7d8d704a5e048417c1f5c318846fedba
Alex Musayev <alex.musayev@gmail.com>
2023-07-21 10:03:06 +0200

    Test `LitterboxNormalizer`

--- spec/normalizers/litterbox_spec.rb
+++ spec/normalizers/litterbox_spec.rb
@@ -1,34 +1,58 @@
 require "rails_helper"
 require "support/shared_examples_a_normalizer"
 
-# class LitterboxTest < Minitest::Test
-#   include FeedTestHelper
+RSpec.describe LitterboxNormalizer do
+  subject(:subject_name) { described_class }
 
-#   def setup
-#     super
+  context "with normal content" do
+    it_behaves_like "a normalizer" do
+      let(:feed) do
+        create(
+          :feed,
+          name: "litterbox",
+          loader: "http",
+          processor: "wordpress",
+          normalizer: "litterbox",
+          url: "https://www.litterboxcomics.com/feed/"
+        )
+      end
 
-#     stub_request(:get, "https://www.litterboxcomics.com/claw-machine/")
-#       .to_return(body: file_fixture("feeds/litterbox/post.html").read)
+      let(:feed_fixture) { "feeds/litterbox/feed.xml" }
+      let(:normalized_fixture) { "feeds/litterbox/normalized.json" }
 
-#     stub_request(:get, "https://www.litterboxcomics.com/claw-machine-bonus/")
-#       .to_return(body: file_fixture("feeds/litterbox/bonus_panel.html").read)
-#   end
+      before do
+        stub_request(:get, "https://www.litterboxcomics.com/claw-machine/")
+          .to_return(body: file_fixture("feeds/litterbox/post.html").read)
 
-#   def feed_config
-#     {
-#       name: "litterbox",
-#       loader: "http",
-#       processor: "wordpress",
-#       normalizer: "litterbox",
-#       url: "https://www.litterboxcomics.com/feed/"
-#     }
-#   end
+        stub_request(:get, "https://www.litterboxcomics.com/claw-machine-bonus/")
+          .to_return(body: file_fixture("feeds/litterbox/bonus_panel.html").read)
+      end
+    end
+  end
 
-#   def source_fixture_path
-#     "feeds/litterbox/feed.xml"
-#   end
+  context "with slides" do
+    it_behaves_like "a normalizer" do
+      let(:feed) do
+        create(
+          :feed,
+          name: "litterbox",
+          loader: "http",
+          processor: "wordpress",
+          normalizer: "litterbox",
+          url: "https://www.litterboxcomics.com/feed/"
+        )
+      end
 
-#   def expected_fixture_path
-#     "feeds/litterbox/entry.json"
-#   end
-# end
+      let(:feed_fixture) { "feeds/litterbox/slides/feed.xml" }
+      let(:normalized_fixture) { "feeds/litterbox/slides/normalized.json" }
+
+      before do
+        stub_request(:get, "https://www.litterboxcomics.com/worlds-collide/")
+          .to_return(body: file_fixture("feeds/litterbox/slides/post.html").read)
+
+        stub_request(:get, "https://www.litterboxcomics.com/worlds-collide-bonus/")
+          .to_return(body: file_fixture("feeds/litterbox/slides/bonus_panel.html").read)
+      end
+    end
+  end
+end
