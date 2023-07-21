8e034a8b4ca9add0b4e0805db7fa51a470f8f4a3
Alex Musayev <alex.musayev@gmail.com>
2023-07-21 10:33:32 +0200

    Test `OglafNormalizer`

--- spec/normalizers/oglaf_normalizer_spec.rb
+++ spec/normalizers/oglaf_normalizer_spec.rb
@@ -4,44 +4,22 @@ require "support/shared_examples_a_normalizer"
 RSpec.describe OglafNormalizer do
   subject(:subject_name) { described_class }
 
-  # TBD
+  it_behaves_like "a normalizer" do
+    let(:feed) do
+      create(
+        :feed,
+        name: "oglaf",
+        loader: "http",
+        processor: "rss",
+        normalizer: "oglaf",
+        url: "https://feeds.feedburner.com/oatmealfeed",
+        import_limit: 2
+      )
+    end
+
+    before do
+      stub_request(:get, /www.oglaf.com/)
+        .to_return(body: file_fixture("feeds/oglaf/post.html").read)
+    end
+  end
 end
-
-# class OglafNormalizerTest < Minitest::Test
-#   include NormalizerTestHelper
-
-#   def subject
-#     OglafNormalizer
-#   end
-
-#   def processor
-#     RssProcessor
-#   end
-
-#   def sample_data_file
-#     "feed_oglaf.xml"
-#   end
-
-#   def setup
-#     super
-#     sample_post = sample_file("post_oglaf.html")
-#     stub_request(:get, /www.oglaf.com/).to_return(body: sample_post)
-#   end
-
-#   def expected
-#     NormalizedEntity.new(
-#       feed_id: feed.id,
-#       uid: "https://www.oglaf.com/thesitter/",
-#       link: "https://www.oglaf.com/thesitter/",
-#       published_at: DateTime.parse("2019-08-18 00:00:00 +0000"),
-#       text: "Legend of the Sitter - https://www.oglaf.com/thesitter/",
-#       attachments: ["https://media.oglaf.com/comic/thesitter.jpg"],
-#       comments: ["formerly the baby sitter"],
-#       validation_errors: []
-#     )
-#   end
-
-#   def test_first_entity
-#     assert_equal(expected, normalized.first)
-#   end
-# end
