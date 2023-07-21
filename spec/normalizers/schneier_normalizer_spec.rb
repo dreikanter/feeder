8f434979ee8bec8644c3906f9776fa826e17acff
Alex Musayev <alex.musayev@gmail.com>
2023-07-21 11:12:03 +0200

    Test `SchneierNormalizer`

--- spec/normalizers/schneier_normalizer_spec.rb
+++ spec/normalizers/schneier_normalizer_spec.rb
@@ -4,32 +4,17 @@ require "support/shared_examples_a_normalizer"
 RSpec.describe SchneierNormalizer do
   subject(:subject_name) { described_class }
 
-  # TBD
+  it_behaves_like "a normalizer" do
+    let(:feed) do
+      create(
+        :feed,
+        name: "schneier",
+        url: "https://www.schneier.com/blog/atom.xml",
+        loader: "http",
+        processor: "atom",
+        normalizer: "schneier",
+        import_limit: 2
+      )
+    end
+  end
 end
-
-# class SchneierNormalizerTest < Minitest::Test
-#   include NormalizerTestHelper
-
-#   def subject
-#     SchneierNormalizer
-#   end
-
-#   def processor
-#     AtomProcessor
-#   end
-
-#   def sample_data_file
-#     "feed_schneier.xml"
-#   end
-
-#   def expected
-#     entity = JSON.parse(file_fixture("feeds/schneier/entity.json").read)
-#     value = entity["published_at"]
-#     entity["published_at"] = DateTime.parse(value) if value
-#     NormalizedEntity.new(**entity.symbolize_keys.merge(feed_id: feed.id))
-#   end
-
-#   def test_normalized_sample
-#     assert_equal(expected, normalized.first)
-#   end
-# end
