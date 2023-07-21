a26bfedded34fd43ce88f79683678a8d9c2a0368
Alex Musayev <alex.musayev@gmail.com>
2023-07-21 09:52:12 +0200

    Test `InfiniteimmortalbensNormalizer`

--- spec/normalizers/infiniteimmortalbens_normalizer_spec.rb
+++ spec/normalizers/infiniteimmortalbens_normalizer_spec.rb
@@ -4,53 +4,20 @@ require "support/shared_examples_a_normalizer"
 RSpec.describe InfiniteimmortalbensNormalizer do
   subject(:subject_name) { described_class }
 
-  # TBD
+  it_behaves_like "a normalizer" do
+    let(:feed) do
+      create(
+        :feed,
+        name: "infiniteimmortalbens",
+        loader: "http",
+        processor: "rss",
+        normalizer: "infiniteimmortalbens",
+        url: "https://infiniteimmortalbens.com/feed/",
+        import_limit: 2
+      )
+    end
+
+    let(:feed_fixture) { "feeds/infiniteimmortalbens/feed.xml" }
+    let(:normalized_fixture) { "feeds/infiniteimmortalbens/normalized.json" }
+  end
 end
-
-# class InfiniteimmortalbensNormalizerTest < Minitest::Test
-#   include NormalizerTestHelper
-
-#   def subject
-#     InfiniteimmortalbensNormalizer
-#   end
-
-#   def processor
-#     RssProcessor
-#   end
-
-#   def sample_data_file
-#     "feed_infiniteimmortalbens.xml".freeze
-#   end
-
-#   def test_text
-#     normalized.each do |entity|
-#       assert_not(entity.text.empty?)
-#     end
-#   end
-
-#   def test_comments
-#     normalized.each do |entity|
-#       assert(entity.comments.empty?)
-#     end
-#   end
-
-#   def test_valid_link
-#     normalized.each do |entity|
-#       Addressable::URI.parse(entity.link)
-#     end
-#   end
-
-#   def test_published_at
-#     normalized.each do |entity|
-#       assert(entity.published_at.is_a?(DateTime))
-#     end
-#   end
-
-#   def test_attachments
-#     normalized.each do |entity|
-#       result = entity.attachments
-#       assert(result.is_a?(Array))
-#       assert(result.any?)
-#     end
-#   end
-# end
