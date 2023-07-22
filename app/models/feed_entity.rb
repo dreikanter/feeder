08a970388d7ac1d0d512224b57fbc5c7fe3aeeda
Alex Musayev <alex.musayev@gmail.com>
2023-07-22 16:37:53 +0200

    Replace `FeedEntity` with a Data object

--- app/models/feed_entity.rb
+++ app/models/feed_entity.rb
@@ -1,9 +1,1 @@
-class FeedEntity
-  attr_reader :uid, :content, :feed
-
-  def initialize(uid:, content:, feed:)
-    @uid = uid.to_s
-    @content = content
-    @feed = feed
-  end
-end
+FeedEntity = Data.define(:uid, :content, :feed)
