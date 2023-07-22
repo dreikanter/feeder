d5c276c2a849ec7ba5fbfe4ca7118b8bf3f0609e
Alex Musayev <alex.musayev@gmail.com>
2023-07-22 16:37:53 +0200

    Replace `FeedEntity` with a Data object

--- spec/models/feed_entity_spec.rb
+++ spec/models/feed_entity_spec.rb
@@ -3,12 +3,9 @@ require "rails_helper"
 RSpec.describe FeedEntity do
   subject(:model) { described_class }
 
-  let(:instance) { model.new(uid: 123, content: "CONTENT", feed: "FEED") }
+  let(:instance) { model.new(uid: "UID", content: "CONTENT", feed: "FEED") }
 
-  it "stringifies uid" do
-    expect(instance.uid).to eq("123")
-  end
-
+  it { expect(instance.uid).to eq("UID") }
   it { expect(instance.content).to eq("CONTENT") }
   it { expect(instance.feed).to eq("FEED") }
 end
