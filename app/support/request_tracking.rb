dee0da5fec24cb1033bc31666671f349eba558cc
Alex Musayev <alex.musayev@gmail.com>
2023-07-26 13:17:42 +0300

    Stringify Hobeybadger breadcrumbs metadata (#506)
    * Stringify Hobeybadger breadcrumbs metadata
    
    * Stringify Hobeybadger breadcrumbs metadata

--- app/support/request_tracking.rb
+++ app/support/request_tracking.rb
@@ -2,6 +2,9 @@
 #
 #   HTTP.use(:request_tracking).get("https://example.com/")
 #
+# SEE: https://github.com/httprb/http/blob/main/lib/http/request.rb
+# SEE: https://github.com/httprb/http/blob/main/lib/http/response.rb
+#
 class RequestTracking < HTTP::Feature
   HTTP::Options.register_feature(:request_tracking, self)
 
@@ -16,35 +19,29 @@ class RequestTracking < HTTP::Feature
   end
 
   def on_error(request, error)
-    breadcrumb("HTTP Request Error", request_metadata(request).merge("error" => error.inspect))
+    breadcrumb("HTTP Request Error", request_metadata(request).merge(error: error.inspect))
   end
 
   private
 
-  def breadcrumb(message, metadata = {})
-    Honeybadger.add_breadcrumb(message, metadata: ensure_primitive_values(metadata), category: "request")
+  def breadcrumb(message, metadata)
+    Honeybadger.add_breadcrumb(message, category: "request", metadata: metadata)
   end
 
-  def ensure_primitive_values(metadata)
-    metadata.to_h { [_1.to_s, _2.to_s] }
-  end
-
-  # SEE: https://github.com/httprb/http/blob/main/lib/http/request.rb
   def request_metadata(request)
     {
-      verb: request.verb,
+      verb: request.verb.to_s,
       uri: request.uri.to_s,
-      headers: request.headers.to_h.to_json
-    }.as_json
+      headers: JSON.pretty_generate(request.headers.to_h)
+    }
   end
 
-  # SEE: https://github.com/httprb/http/blob/main/lib/http/response.rb
   def response_metadata(response)
     {
-      status: response.status,
-      headers: response.headers.to_h.to_json,
-      version: response.version,
-      proxy_headers: response.proxy_headers.to_json
-    }.as_json
+      code: response.status.code,
+      headers: JSON.pretty_generate(response.headers.to_h),
+      version: response.version.to_s,
+      proxy_headers: JSON.pretty_generate(response.proxy_headers.to_h)
+    }
   end
 end
