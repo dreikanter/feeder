8945ff74c9b5b3fd86075f879a5a40faeb30bd47
Alex Musayev <alex.musayev@gmail.com>
2023-07-26 11:21:22 +0200

    Stringify Hobeybadger breadcrumbs metadata

--- app/support/request_tracking.rb
+++ app/support/request_tracking.rb
@@ -2,49 +2,51 @@
 #
 #   HTTP.use(:request_tracking).get("https://example.com/")
 #
+# SEE: https://github.com/httprb/http/blob/main/lib/http/request.rb
+# SEE: https://github.com/httprb/http/blob/main/lib/http/response.rb
+#
 class RequestTracking < HTTP::Feature
   HTTP::Options.register_feature(:request_tracking, self)
 
   def wrap_request(request)
-    breadcrumb("HTTP Request", request_metadata(request))
+    Honeybadger.add_breadcrumb(
+      "HTTP Request",
+      category: "request",
+      metadata: {
+        verb: request.verb.to_s,
+        uri: request.uri.to_s,
+        headers: request.headers.to_h.to_json
+      }
+    )
+
     request
   end
 
   def wrap_response(response)
-    breadcrumb("HTTP Response", response_metadata(response))
+    Honeybadger.add_breadcrumb(
+      "HTTP Response",
+      category: "request",
+      metadata: {
+        status: response.status.to_s,
+        headers: response.headers.to_h.to_json,
+        version: response.version.to_s,
+        proxy_headers: response.proxy_headers.to_json
+      }
+    )
+
     response
   end
 
   def on_error(request, error)
-    breadcrumb("HTTP Request Error", request_metadata(request).merge("error" => error.inspect))
-  end
-
-  private
-
-  def breadcrumb(message, metadata = {})
-    Honeybadger.add_breadcrumb(message, metadata: ensure_primitive_values(metadata), category: "request")
-  end
-
-  def ensure_primitive_values(metadata)
-    metadata.to_h { [_1.to_s, _2.to_s] }
-  end
-
-  # SEE: https://github.com/httprb/http/blob/main/lib/http/request.rb
-  def request_metadata(request)
-    {
-      verb: request.verb,
-      uri: request.uri.to_s,
-      headers: request.headers.to_h.to_json
-    }.as_json
-  end
-
-  # SEE: https://github.com/httprb/http/blob/main/lib/http/response.rb
-  def response_metadata(response)
-    {
-      status: response.status,
-      headers: response.headers.to_h.to_json,
-      version: response.version,
-      proxy_headers: response.proxy_headers.to_json
-    }.as_json
+  Honeybadger.add_breadcrumb(
+      "HTTP Request Error",
+      category: "request",
+      metadata: {
+        error: error.inspect,
+        verb: request.verb.to_s,
+        uri: request.uri.to_s,
+        headers: request.headers.to_h.to_json
+      }
+    )
   end
 end
