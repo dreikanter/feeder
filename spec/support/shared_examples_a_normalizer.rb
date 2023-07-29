a151f5fdc65975f4fb7fb178060705d22d1befa1
Alex Musayev <alex.musayev@gmail.com>
2023-07-29 19:05:47 +0200

    Make request stub for the feed URL optional

--- spec/support/shared_examples_a_normalizer.rb
+++ spec/support/shared_examples_a_normalizer.rb
@@ -16,6 +16,9 @@ RSpec.shared_examples "a normalizer" do
   let(:expected_posts_data) { JSON.parse(file_fixture(normalized_fixture).read) }
   let(:imported_post_ids) { PostsImporter.new(feed).import }
 
+  let(:feed_fixture_path) { File.join(file_fixture_path, feed_fixture) }
+  let(:normalized_fixture_path) { File.join(file_fixture_path, normalized_fixture) }
+
   let(:imported_posts_data) do
     Post.where(feed: feed, uid: imported_post_ids).as_json.map do |attributes|
       attributes.slice(*%w[
@@ -34,22 +37,23 @@ RSpec.shared_examples "a normalizer" do
   before do
     Feed.delete_all
     travel_to(DateTime.parse("2023-06-17 01:00:00 +0000"))
-
-    stub_request(:get, feed.url)
-      .to_return(body: file_fixture(feed_fixture).read)
-      .times(1).then.to_raise("repeated loader call")
+    stub_feed_url if File.exist?(feed_fixture_path)
   end
 
   it { expect(feed.normalizer_class).to eq(described_class) }
 
   it "matches data snapshot" do
-    fixture_path = File.join(file_fixture_path, normalized_fixture)
-
-    unless File.exist?(fixture_path)
-      File.write(fixture_path, JSON.pretty_generate(imported_posts_data))
+    unless File.exist?(normalized_fixture_path)
+      File.write(normalized_fixture_path, JSON.pretty_generate(imported_posts_data))
       skip
     end
 
     expect(imported_posts_data).to eq(expected_posts_data)
+  end
+
+  def stub_feed_url
+    stub_request(:get, feed.url)
+      .to_return(body: file_fixture(feed_fixture).read)
+      .times(1).then.to_raise("repeated loader call")
   end
 end
