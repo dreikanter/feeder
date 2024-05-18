e4953a53ff068ada8bc229fd5f80f2c917c4d095
Alex Musayev <alex.musayev@gmail.com>
2024-05-18 18:06:55 +0200

    Attempt to extract direct URL

--- app/normalizers/pluralistic_normalizer.rb
+++ app/normalizers/pluralistic_normalizer.rb
@@ -6,14 +6,26 @@ class PluralisticNormalizer < RssNormalizer
   end
 
   def attachments
-    [cover_image]
+    [extract_direct_image_url(cover_image)]
+  rescue StandardError
+    super
   end
 
   private
 
+  def extract_direct_image_url(url)
+    parsed_uri = URI.parse(url)
+    return url unless parsed_uri.host =~ /^i\d+\.wp\.com$/.freeze
+
+    original_path = parsed_uri.path
+    original_query = parsed_uri.query
+    original_host = original_path.split("/")[1]
+
+    original_url = "https://#{original_host}#{original_path.sub("/#{original_host}", '')}"
+    original_query ? "#{original_url}?#{original_query}" : original_url
+  end
+
   def cover_image
     Html.first_image_url(http.get(content.link).to_s)
-  rescue StandardError
-    nil
   end
 end
