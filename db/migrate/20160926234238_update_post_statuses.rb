0646fb7ffa2d0ca3238a27912db0d41e66984b6c
Alex Musayev <alex.musayev@gmail.com>
2024-07-28 16:07:19 +0200

    Remove model reference from migration

--- db/migrate/20160926234238_update_post_statuses.rb
+++ db/migrate/20160926234238_update_post_statuses.rb
@@ -1,6 +1,11 @@
 class UpdatePostStatuses < ActiveRecord::Migration[5.0]
   def up
-    Post.idle.where.not(freefeed_post_id: nil).update_all(status: :published)
+    # Replace "idle" status with "published"
+    execute <<-SQL
+      UPDATE posts
+      SET status = 2
+      WHERE status = 0 AND freefeed_post_id IS NOT NULL
+    SQL
   end
 
   def down
