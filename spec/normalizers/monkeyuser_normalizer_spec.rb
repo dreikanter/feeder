ee63f292200da1d919ac9a0104803b844b648f21
Alex Musayev <alex.musayev@gmail.com>
2024-08-09 19:19:07 +0200

    Refresh `MonkeyuserNormalizer` and update the feed configuration

--- spec/normalizers/monkeyuser_normalizer_spec.rb
+++ spec/normalizers/monkeyuser_normalizer_spec.rb
@@ -2,19 +2,40 @@ require "rails_helper"
 require "support/shared_examples_a_normalizer"
 
 RSpec.describe MonkeyuserNormalizer do
-  subject(:subject_name) { described_class }
+  subject(:normalizer) { described_class }
 
-  it_behaves_like "a normalizer" do
-    let(:feed) do
-      create(
-        :feed,
-        name: "monkeyuser",
-        url: "https://www.monkeyuser.com/feed.xml",
-        loader: "http",
-        processor: "feedjira",
-        normalizer: "monkeyuser",
-        import_limit: 2
-      )
-    end
+  let(:feed) do
+    build(
+      :feed,
+      name: "monkeyuser",
+      url: "https://www.monkeyuser.com/feed.xml",
+      loader: "http",
+      processor: "feedjira",
+      normalizer: "monkeyuser",
+      import_limit: 2
+    )
+  end
+
+  let(:feed_content) { file_fixture("feeds/#{feed.name}/feed.xml").read }
+  let(:feed_entities) { feed.processor_class.new(content: feed_content, feed: feed).process[0..2] }
+  let(:post_content) { file_fixture("feeds/#{feed.name}/post.html").read }
+
+  let(:expected) do
+    json = file_fixture("feeds/#{feed.name}/normalized.json").read
+    JSON.parse(json).map { _1.merge("feed_id" => feed.id) }
+  end
+
+  before do
+    stub_request(:get, %r{^https://www.monkeyuser.com/}).to_return(body: post_content)
+  end
+
+  it "resolves normalizer class" do
+    expect(feed.normalizer_class).to eq(normalizer)
+  end
+
+  it "matches the expected result" do
+    result = feed_entities.map { normalizer.call(_1).as_json }
+
+    expect(result).to eq(expected)
   end
 end
