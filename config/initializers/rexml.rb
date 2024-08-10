0c8e2df724d1d8c31a507b1b302f1378960dddfc
Alex Musayev <alex.musayev@gmail.com>
2024-08-10 12:36:56 +0200

    Update REXML limits

--- config/initializers/rexml.rb
+++ config/initializers/rexml.rb
@@ -1,2 +1,7 @@
+require "rexml/document"
+
+# Default is 10MB
+REXML::Security.entity_expansion_text_limit = 100_000_000
+
 # Default is 10K
-REXML::Security.entity_expansion_text_limit = 10_000_000
+REXML::Security.entity_expansion_limit = 100_000
