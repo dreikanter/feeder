357b5d28a97c0dc7b0a41698f2849c255a869209
Alex Musayev <alex.musayev@gmail.com>
2024-08-10 12:02:29 +0200

    Move `entity_expansion_text_limit` to initializer

--- app/processors/rss_processor.rb
+++ app/processors/rss_processor.rb
@@ -3,9 +3,6 @@ require "rexml/document"
 
 class RssProcessor < BaseProcessor
   def process
-    # TBD
-    REXML::Security.entity_expansion_text_limit = 10_000_000
-
     items = RSS::Parser.parse(content).try(:items)
     (items || []).map { build_entity(_1.link, _1) }
   end
---
+++ config/initializers/rexml.rb
@@ -0,0 +1,2 @@
+# Default is 10K
+REXML::Security.entity_expansion_text_limit = 10_000_000
