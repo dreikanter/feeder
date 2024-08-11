fd029ac7d1a187a16c9e0fa105efcea1326307f4
Alex Musayev <alex.musayev@gmail.com>
2024-08-11 15:47:11 +0200

    Require only necessary engines

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,18 @@
 require_relative "boot"
 
 require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+
+# Picking only the required gems
+# SEE: https://github.com/rails/rails/blob/main/railties/lib/rails/all.rb
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
+require "action_view/railtie"
 # require "action_mailer/railtie"
+require "active_job/railtie"
+# require "action_cable/engine"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
-# require "action_cable/engine"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
