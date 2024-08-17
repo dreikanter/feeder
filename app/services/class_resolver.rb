7b03afc8e37eb5671873474ca1a1a166431689e5
Alex Musayev <alex.musayev@gmail.com>
2024-08-17 14:57:44 +0200

    Add comments to `ClassResolver`

--- app/services/class_resolver.rb
+++ app/services/class_resolver.rb
@@ -1,11 +1,18 @@
+# Resolver a class from the class name and optional suffix.
+#
 class ClassResolver
   attr_reader :class_name, :suffix
 
+  # @param [String] a string representing the target class name with words
+  #   separated by underscores
+  # @param suffix: [String] optional suffix for the class name
   def initialize(class_name, suffix: nil)
     @class_name = class_name
     @suffix = suffix
   end
 
+  # @return [Class]
+  # @raise [NameError] if the target class is missing
   def resolve
     [class_name, suffix].join("_").classify.constantize
   end
