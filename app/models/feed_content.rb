0e166a081470fea1b32cffea1ceb9e0f035774de
Alex Musayev <alex.musayev@gmail.com>
2024-08-18 02:04:05 +0200

    Add more fields

--- app/models/feed_content.rb
+++ app/models/feed_content.rb
@@ -4,9 +4,11 @@
 # @see BaseLoader
 #
 class FeedContent
-  attr_reader :raw_content
+  attr_reader :raw_content, :imported_at, :import_duration
 
-  def initialize(raw_content)
+  def initialize(raw_content:, imported_at:, import_duration:)
     @raw_content = raw_content
+    @imported_at = imported_at
+    @import_duration = import_duration
   end
 end
