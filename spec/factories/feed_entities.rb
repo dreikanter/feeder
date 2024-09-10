ef40149dfb5a469e46ff3a1abdb4964406f63cb8
Alex Musayev <alex.musayev@gmail.com>
2024-09-10 11:58:08 +0200

    Add `FeedEntity` factory

---
+++ spec/factories/feed_entities.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :feed_entity, class: "FeedEntity" do
+    feed { build(:feed) }
+    uid { "https://example.com/1" }
+    content { "CONTENT" }
+
+    initialize_with do
+      new(
+        feed: feed,
+        uid: uid,
+        content: content
+      )
+    end
+  end
+end
