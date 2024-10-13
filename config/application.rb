6f0841000dd473e65910d58088d4bccd47ec424e
Alex Musayev <alex.musayev@gmail.com>
2024-10-13 13:30:03 +0200

    Run `rails app:update`

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,17 @@
 require_relative "boot"
 
 require "rails"
-
-# Picking only the required gems
-# SEE: https://github.com/rails/rails/blob/main/railties/lib/rails/all.rb
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-require "action_view/railtie"
 # require "action_mailer/railtie"
-require "active_job/railtie"
-# require "action_cable/engine"
 # require "action_mailbox/engine"
 # require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
