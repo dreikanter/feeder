c2c90bff40cc88dc8317b7def32996850add6ea4
Alex Musayev <alex.musayev@gmail.com>
2024-10-18 12:28:52 +0200

    Set up dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resource :session, only: %i[new create, destroy]
+  resource :session, only: %i[new create destroy]
   resources :passwords, param: :token, only: %i[new create edit update]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", :as => :rails_health_check
+
+  root "dashboards#show"
 end
