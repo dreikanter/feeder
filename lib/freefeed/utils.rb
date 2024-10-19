4d40dcb61461a2825af5bff75c044963cb3e8f17
Alex Musayev <alex.musayev@gmail.com>
2024-10-19 20:14:13 +0200

    Fix method calls

--- lib/freefeed/utils.rb
+++ lib/freefeed/utils.rb
@@ -1,11 +1,21 @@
 module Freefeed
   module Utils
     def authenticated_request(request_method, path, params = {})
-      AuthenticatedRequest.new(self, request_method, path, params).call
+      AuthenticatedRequest.new(
+        client: self,
+        request_method: request_method,
+        path: path,
+        options: params
+      ).call
     end
 
     def request(request_method, path, params = {})
-      Request.new(self, request_method, path, params).call
+      Request.new(
+        client: self,
+        request_method: request_method,
+        path: path,
+        options: params
+      ).call
     end
   end
 end
