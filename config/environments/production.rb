27e0e65a1aea2e1e38fb0cb459180f92f434c5ec
Alex Musayev <alex.musayev@gmail.com>
2024-10-19 15:43:27 +0300

    F2: Make forms look nice (#592)
    * Update readme with `puma-dev` setup instruction
    
    * Link Bootstrap assets
    
    * Use nice forms styling
    
    * Enable DNS rebinding protection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = ["frf.im", "www.frf.im"]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
