b2278181d05765f76fcd8fe94a0ce561135df73b
Alex Musayev <alex.musayev@gmail.com>
2024-10-19 19:55:28 +0200

    Use HTTP client directly

--- lib/freefeed/request.rb
+++ lib/freefeed/request.rb
@@ -1,6 +1,9 @@
 module Freefeed
   class Request
-    include HttpClient
+    DEFAULT_OPTIONS = {
+      http_max_hops: 3,
+      http_timeout_seconds: 5
+    }.freeze
 
     attr_reader :client, :request_method, :path, :options
 
@@ -39,6 +42,14 @@ module Freefeed
         accept: "*/*",
         user_agent: "feeder"
       }
+    end
+
+    def http
+      HTTP.follow(max_hops: option(:http_max_hops)).timeout(option(:http_timeout_seconds))
+    end
+
+    def option(option_name)
+      options.fetch(option_name) { DEFAULT_OPTIONS.fetch(option_name) }
     end
   end
 end
