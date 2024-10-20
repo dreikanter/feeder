527b872470326ab86f48d3b0b51801fe601d6f7a
Alex Musayev <alex.musayev@gmail.com>
2024-10-20 15:21:09 +0200

    Use consistent terminology

--- lib/freefeed/utils.rb
+++ lib/freefeed/utils.rb
@@ -1,21 +1,21 @@
 module Freefeed
   module Utils
-    def authenticated_request(request_method, path, params = {})
+    def authenticated_request(request_method, path, payload = {})
       AuthenticatedRequest.new(
         client: self,
         request_method: request_method,
         path: path,
-        options: params
-      ).call
+        payload: payload
+      ).perform
     end
 
-    def request(request_method, path, params = {})
+    def request(request_method, path, payload = {})
       Request.new(
         client: self,
         request_method: request_method,
         path: path,
-        options: params
-      ).call
+        payload: payload
+      ).perform
     end
   end
 end
