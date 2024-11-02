43c57d685d5528f5edf34a4da43001f10c635960
Alex Musayev <alex.musayev@gmail.com>
2024-11-02 17:32:37 +0100

    Test coverage

--- spec/services/post_publisher_spec.rb
+++ spec/services/post_publisher_spec.rb
@@ -8,11 +8,21 @@ RSpec.describe PostPublisher do
     )
   end
 
+  let(:feed) { create(:feed, name: "sample_feed") }
+
   describe "#publish" do
     it "publishes a post" do
-      post = create(:post, state: "enqueued", attachments: [], comments: [])
+      post = create(
+        :post,
+        feed: feed,
+        state: "enqueued",
+        attachments: [],
+        comments: []
+      )
+
       publisher = described_class.new(post: post, freefeed_client: freefeed_client)
 
+      # Create post
       stub_request(:post, "https://freefeed.test/v1/posts")
         .with(
           body: {
@@ -21,7 +31,7 @@ RSpec.describe PostPublisher do
               "attachments" => []
             },
             "meta" => {
-              "feeds" => ["sample"]
+              "feeds" => [feed.name]
             }
           }.to_json,
           headers: {
@@ -45,9 +55,90 @@ RSpec.describe PostPublisher do
           }
         )
 
-      expect { publisher.publish }.to change { post.reload.slice("state", "freefeed_post_id") }.
-        from("state" => "enqueued", "freefeed_post_id" => nil).
-        to("state" => "published", "freefeed_post_id" => "TEST_POST_ID")
+      expect { publisher.publish }.to \
+        change { post.reload.slice("state", "freefeed_post_id") }
+        .from("state" => "enqueued", "freefeed_post_id" => nil)
+        .to("state" => "published", "freefeed_post_id" => "TEST_POST_ID")
+    end
+
+    it "publishes attachments" do
+      post = create(
+        :post,
+        feed: feed,
+        state: "enqueued",
+        attachments: ["https://example.com/image.jpg"],
+        comments: []
+      )
+
+      publisher = described_class.new(post: post, freefeed_client: freefeed_client)
+
+      # Download attached image
+      stub_request(:get, "https://example.com/image.jpg")
+        .to_return(
+          status: 200,
+          body: file_fixture("image_1x1.jpg").read,
+          headers: {"Content-Type" => "image/jpeg"}
+        )
+
+      # Create attachment
+      stub_request(:post, "https://freefeed.test/v1/attachments")
+        .to_return(
+          status: 200,
+          body: {"attachments" => {"id" => "TEST_ATTACHMENT_ID"}}.to_json,
+          headers: {
+            "Content-Type" => "application/json; charset=utf-8"
+          }
+        )
+
+      # Create post
+      stub_request(:post, "https://freefeed.test/v1/posts")
+        .with(
+          body: {
+            "post" => {
+              "body" => "Sample post text",
+              "attachments" => ["TEST_ATTACHMENT_ID"]
+            },
+            "meta" => {
+              "feeds" => [feed.name]
+            }
+          }.to_json,
+          headers: {
+            "Accept" => "*/*",
+            "Authorization" => "Bearer TEST_TOKEN",
+            "Connection" => "close",
+            "Content-Type" => "application/json; charset=utf-8",
+            "Host" => "freefeed.test",
+            "User-Agent" => "feeder"
+          }
+        )
+        .to_return(
+          status: 200,
+          body: {
+            "posts" => {
+              "id" => "TEST_POST_ID"
+            }
+          }.to_json,
+          headers: {
+            "Content-Type" => "application/json; charset=utf-8"
+          }
+        )
+
+      expect { publisher.publish }.to \
+        change { post.reload.slice("state", "freefeed_post_id") }
+        .from("state" => "enqueued", "freefeed_post_id" => nil)
+        .to("state" => "published", "freefeed_post_id" => "TEST_POST_ID")
+    end
+
+    it "publishes comments" do
+    end
+
+    it "handles error response" do
+    end
+
+    it "handles request timeout" do
+    end
+
+    it "handles HTTP client exception" do
     end
   end
 end
